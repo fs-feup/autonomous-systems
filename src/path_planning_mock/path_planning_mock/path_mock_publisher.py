59c680abf7cdb9fc80a5e6fd4cfd1fa3461355a0
JoaoAMarinho <joao.andre.marinho@gmail.com>
2023-02-13 22:33:40 +0000

    style: rename topic

--- src/path_planning_mock/path_planning_mock/path_mock_publisher.py
+++ src/path_planning_mock/path_planning_mock/path_mock_publisher.py
@@ -8,7 +8,7 @@ class PathMockPublisher(Node):
 
     def __init__(self):
         super().__init__('path_mock_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'path_mock', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
