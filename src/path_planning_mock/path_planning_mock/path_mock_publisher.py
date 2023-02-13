51d3e2df37a58ec4c74b5f227ee04ba14d8e1c08
JoaoAMarinho <joao.andre.marinho@gmail.com>
2023-02-13 23:17:08 +0000

    feat: implement custom interfaces pkg

--- src/path_planning_mock/path_planning_mock/path_mock_publisher.py
+++ src/path_planning_mock/path_planning_mock/path_mock_publisher.py
@@ -1,23 +1,24 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from custom_interfaces.msg import Point2d as Point
 
 
 class PathMockPublisher(Node):
 
     def __init__(self):
         super().__init__('path_mock_publisher')
-        self.publisher_ = self.create_publisher(String, 'path_mock', 10)
+        self.publisher_ = self.create_publisher(Point, 'path_mock', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.i = 0.
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Point()
+        msg.x = self.i
+        msg.y = self.i + 1.0
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info(f"Publishing: X:{msg.x}, Y:{msg.y}")
         self.i += 1
 
 
