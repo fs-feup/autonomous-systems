14113b5fe6260d52a42d55ef74e874127f62fe39
Marcelo Couto <marhcouto1101@gmail.com>
2023-03-07 21:57:42 +0000

    feat(loc-map): implemented localization and mapping publisher (#57)
    * feat(loc-map): implemented localization and mapping publisher
    
    * fix: fixed cpplint

--- src/localization-mapping/loc_map/include/loc_map/lm_publisher.hpp
+++ src/localization-mapping/loc_map/include/loc_map/lm_publisher.hpp
@@ -4,32 +4,57 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <typeinfo>
 
+#include "custom_interfaces/msg/cone_array.hpp"
 #include "custom_interfaces/msg/point2d.hpp"
+#include "custom_interfaces/msg/pose.hpp"
+#include "loc_map/data_structures.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
+/**
+ * @brief Class for the node responsible for publishing the localization and the
+ * map of the vehicle
+ *
+ */
 class LMPublisher : public rclcpp::Node {
- private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  rclcpp::TimerBase::SharedPtr _timer;
+  rclcpp::Publisher<custom_interfaces::msg::Pose>::SharedPtr _localization_publisher;
+  rclcpp::Publisher<custom_interfaces::msg::ConeArray>::SharedPtr _mapping_publisher;
+  Localization* _vehicle_localization;
+  Map* _track_map;
+  size_t _count;  // TODO(marhcouto): remove this variable
 
-  void timer_callback() {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-  }
+  /**
+   * @brief timer callback function for publishing the localization and the map
+   *
+   */
+  void _timer_callback();
+
+  /**
+   * @brief publishes the localization ('vehicle_localization') to the topic
+   * vehicle_location
+   *
+   * @param vehicle_pose
+   */
+  void _publish_localization(Localization vehicle_localization);
+
+  /**
+   * @brief publishes the map ('track_map') to the topic track_map
+   *
+   * @param track_map
+   */
+  void _publish_map(Map track_map);
 
  public:
-  LMPublisher() : Node("loc_map_publisher"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-        500ms, std::bind(&LMPublisher::timer_callback, this));
-  }
+  /**
+   * @brief Construct a new LMPublisher object
+   *
+   */
+  LMPublisher(Localization* vehicle_localization, Map* track_map);
 };
 
 #endif  // SRC_LOCALIZATION_MAPPING_LOC_MAP_INCLUDE_LOC_MAP_LM_PUBLISHER_HPP_
