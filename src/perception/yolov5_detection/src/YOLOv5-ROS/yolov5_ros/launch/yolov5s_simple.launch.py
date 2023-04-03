15a1e37b8442992df276ad3e1b6985c510ca7911
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2023-04-03 17:14:03 +0100

    Cone detection fully functional, custom message topic with bounding box coordinates created but has issues

---
+++ src/perception/yolov5_detection/src/YOLOv5-ROS/yolov5_ros/launch/yolov5s_simple.launch.py
@@ -0,0 +1,21 @@
+import launch
+import launch_ros.actions
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    yolox_ros_share_dir = get_package_share_directory('yolov5_ros')
+
+
+    yolov5_ros = launch_ros.actions.Node(
+        package="yolov5_ros", executable="yolov5_ros",
+        parameters=[
+            {"view_img":True},
+        ],
+
+    )
+    
+    return launch.LaunchDescription([
+        yolov5_ros,
+    ])
