24bb577e9f2c390021ad397281d375b47d3a0ea4
Vitor <vitorjardimbueno@hotmail.com>
2023-06-18 03:04:53 +0100

    code organization

--- src/control/control/mpc.py
+++ src/control/control/mpc.py
@@ -1,136 +1,89 @@
 import numpy as np
-import cvxpy as opt
-from .config import Params
-
-np.seterr(divide="ignore", invalid="ignore")
+from .mpc_utils import compute_path_from_wp, get_ref_trajectory, get_linear_model_matrices
+from .optimizer import Optimizer, Params
+import time
 
 P = Params()
 
-
-def get_linear_model_matrices(x_bar, u_bar):
-    """
-    Computes the LTI approximated state space model x' = Ax + Bu + C
-    """
-
-    x_bar[0]
-    x_bar[1]
-    v = x_bar[2]
-    theta = x_bar[3]
-
-    a = u_bar[0]
-    delta = u_bar[1]
-
-    ct = np.cos(theta)
-    st = np.sin(theta)
-    cd = np.cos(delta)
-    td = np.tan(delta)
-
-    A = np.zeros((P.N, P.N))
-    A[0, 2] = ct
-    A[0, 3] = -v * st
-    A[1, 2] = st
-    A[1, 3] = v * ct
-    A[3, 2] = v * td / P.L
-    A_lin = np.eye(P.N) + P.DT * A
-
-    B = np.zeros((P.N, P.M))
-    B[2, 0] = 1
-    B[3, 1] = v / (P.L * cd**2)
-    B_lin = P.DT * B
-
-    f_xu = np.array([v * ct, v * st, a, v * td / P.L]).reshape(P.N, 1)
-    C_lin = (
-        P.DT
-        * (
-            f_xu - np.dot(A, x_bar.reshape(P.N, 1)) - np.dot(B, u_bar.reshape(P.M, 1))
-        ).flatten()
-    )
-
-    # return np.round(A_lin,6), np.round(B_lin,6), np.round(C_lin,6)
-    return A_lin, B_lin, C_lin
-
-
+# Classes
 class MPC:
-    def __init__(self, N, M, Q, R):
-        """ """
-        self.state_len = N
-        self.action_len = M
-        self.state_cost = Q
-        self.action_cost = R
+    def __init__(self, action, state, path, closest_ind):
+        # State for the car mathematical model [x,y,heading]
+        self.state = state
 
-    def optimize_linearized_model(
-        self,
-        A,
-        B,
-        C,
-        initial_state,
-        target,
-        time_horizon=10,
-        Q=None,
-        R=None,
-        verbose=False,
-    ):
+        # starting guess
+        self.action = action 
+
+        self.opt_u = np.zeros((P.M, P.T))
+
+        # Cost Matrices
+        Q = np.diag([20, 20, 10, 20])  # state error cost
+        np.diag([30, 30, 30, 30])  # state final error cost
+        R = np.diag([10, 10])  # input cost
+        np.diag([10, 10])  # input rate of change cost
+
+        self.optimizer = Optimizer(P.N, P.M, Q, R)
+
+        # Interpolated Path to follow given waypoints
+        self.path = compute_path_from_wp(
+            path,
+            P.path_tick,
+        )
+
+        self.closest_ind = None
+        self.old_closest_ind = closest_ind
+
+        # Sim help vars
+        self.sim_time = 0
+        self.x_history = []
+        self.y_history = []
+        self.v_history = []
+        self.h_history = []
+        self.a_history = []
+        self.d_history = []
+        self.predicted = None
+
+    def run(self):
+        """!
+        @brief MPC run function. Pre-processes the current state, calls the optimizer 
+        and save the actions.
+        @param self The object pointer.
         """
-        Optimisation problem defined for the linearised model,
-        :param A:
-        :param B:
-        :param C:
-        :param initial_state:
-        :param Q:
-        :param R:
-        :param target:
-        :param time_horizon:
-        :param verbose:
-        :return:
-        """
+        time.time()
 
-        assert len(initial_state) == self.state_len
+        # dynamycs w.r.t car frame
+        curr_state = np.array([0, 0, self.state[2], 0])
 
-        if Q is None or R is None:
-            Q = self.state_cost
-            R = self.action_cost
+        # State Matrices
+        A, B, C = get_linear_model_matrices(curr_state, self.action)
+        
+        # Get Reference_traj -> inputs are in worldframe
+        target, _, self.closest_ind = get_ref_trajectory(
+            self.state, self.path, P.VEL, dl=P.path_tick, old_ind=self.old_closest_ind
+        )
 
-        # Create variables
-        x = opt.Variable((self.state_len, time_horizon + 1), name="states")
-        u = opt.Variable((self.action_len, time_horizon), name="actions")
-
-        # Loop through the entire time_horizon and append costs
-        cost_function = []
-
-        for t in range(time_horizon):
-
-            _cost = opt.quad_form(target[:, t + 1] - x[:, t + 1], Q) + opt.quad_form(
-                u[:, t], R
+        x_mpc, u_mpc = self.optimizer.optimize_linearized_model(
+            A,
+            B,
+            C,
+            curr_state,
+            target,
+            time_horizon=P.T,
+            verbose=False,
+        )
+        self.opt_u = np.vstack(
+            (
+                np.array(u_mpc.value[0, :]).flatten(),
+                (np.array(u_mpc.value[1, :]).flatten()),
             )
+        )
+        self.action[:] = [u_mpc.value[0, 0], u_mpc.value[1, 0]]
+        # print("CVXPY Optimization Time: {:.4f}s".format(time.time()-start))
 
-            _constraints = [
-                x[:, t + 1] == A @ x[:, t] + B @ u[:, t] + C,
-                u[0, t] >= -P.MAX_ACC,
-                u[0, t] <= P.MAX_ACC,
-                u[1, t] >= -P.MAX_STEER,
-                u[1, t] <= P.MAX_STEER,
-            ]
-            # opt.norm(target[:, t + 1] - x[:, t + 1], 1) <= 0.1]
-
-            # Actuation rate of change
-            if t < (time_horizon - 1):
-                _cost += opt.quad_form(u[:, t + 1] - u[:, t], R * 1)
-                _constraints += [opt.abs(u[0, t + 1] - u[0, t]) / P.DT <= P.MAX_D_ACC]
-                _constraints += [opt.abs(u[1, t + 1] - u[1, t]) / P.DT <= P.MAX_D_STEER]
-
-            if t == 0:
-                # _constraints += [
-                # opt.norm(target[:, time_horizon] - x[:, time_horizon], 1) <= 0.01,
-                #                x[:, 0] == initial_state]
-                _constraints += [x[:, 0] == initial_state]
-
-            cost_function.append(
-                opt.Problem(opt.Minimize(_cost), constraints=_constraints)
-            )
-
-        # Add final cost
-        problem = sum(cost_function)
-
-        # Minimize Problem
-        problem.solve(verbose=verbose, solver=opt.OSQP)
-        return x, u
+def run_mpc(action, state, path, old_closest_ind):
+    mpc = MPC(action, state, path, old_closest_ind)
+    try:
+        mpc.run()
+        return mpc.action, mpc.closest_ind
+    except Exception:
+        return None, None
