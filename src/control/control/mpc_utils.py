ed2ac76d9505b6c7bd210a8d069093b284fd8edb
Vitor <vitorjardimbueno@hotmail.com>
2023-06-18 03:04:53 +0100

    code organization

--- src/control/control/mpc_utils.py
+++ src/control/control/mpc_utils.py
@@ -78,38 +78,98 @@ def normalize_angle(angle):
 def get_ref_trajectory(state, path, target_v, dl=0.1, old_ind=0):
     """
     For each step in the time horizon
-    modified reference in robot frame
+    modified reference in car frame
     """
+    # initialize variables
     xref = np.zeros((P.N, P.T + 1))
     dref = np.zeros((1, P.T + 1))
     # sp = np.ones((1,T +1))*target_v #speed profile
-    ncourse = path.shape[1]
+
+    path_len = path.shape[1]
+
     ind = get_nn_idx(state, path, old_ind)
     dx = path[0, ind] - state[0]
     dy = path[1, ind] - state[1]
+
+    # first position references
     xref[0, 0] = dx * np.cos(-state[3]) - dy * np.sin(-state[3])  # X
     xref[1, 0] = dy * np.cos(-state[3]) + dx * np.sin(-state[3])  # Y
     xref[2, 0] = target_v  # V
     xref[3, 0] = normalize_angle(path[2, ind] - state[3])  # Theta
+
+    # first steering reference
     dref[0, 0] = 0.0  # Steer operational point should be 0
-    travel = 0.0
+
+    travel = 0.0  # distance traveled based on reference velocity
+
+    # iterate over timesteps in the optimization
     for i in range(1, P.T + 1):
+        # update distance traveled
         travel += abs(target_v) * P.DT
         dind = int(round(travel / dl))
-        if (ind + dind) < ncourse:
+        
+        if (ind + dind) < path_len:
+            # update expected position
             dx = path[0, ind + dind] - state[0]
             dy = path[1, ind + dind] - state[1]
+
+            # position references
             xref[0, i] = dx * np.cos(-state[3]) - dy * np.sin(-state[3])
             xref[1, i] = dy * np.cos(-state[3]) + dx * np.sin(-state[3])
             xref[2, i] = target_v  # sp[ind + dind]
             xref[3, i] = normalize_angle(path[2, ind + dind] - state[3])
+
+            # steering angle 
             dref[0, i] = 0.0
         else:
-            dx = path[0, ncourse - 1] - state[0]
-            dy = path[1, ncourse - 1] - state[1]
+            dx = path[0, path_len - 1] - state[0]
+            dy = path[1, path_len - 1] - state[1]
             xref[0, i] = dx * np.cos(-state[3]) - dy * np.sin(-state[3])
             xref[1, i] = dy * np.cos(-state[3]) + dx * np.sin(-state[3])
-            xref[2, i] = 0.0  # stop? #sp[ncourse - 1]
-            xref[3, i] = normalize_angle(path[2, ncourse - 1] - state[3])
+            xref[2, i] = 0.0  # stop? if not: #sp[ncourse - 1]
+            xref[3, i] = normalize_angle(path[2, path_len - 1] - state[3])
             dref[0, i] = 0.0
     return xref, dref, ind
+
+
+def get_linear_model_matrices(x_bar, u_bar):
+    """
+    Computes the LTI approximated state space model x' = Ax + Bu + C
+    """
+
+    x_bar[0]
+    x_bar[1]
+    v = x_bar[2]
+    theta = x_bar[3]
+
+    a = u_bar[0]
+    delta = u_bar[1]
+
+    ct = np.cos(theta)
+    st = np.sin(theta)
+    cd = np.cos(delta)
+    td = np.tan(delta)
+
+    A = np.zeros((P.N, P.N))
+    A[0, 2] = ct
+    A[0, 3] = -v * st
+    A[1, 2] = st
+    A[1, 3] = v * ct
+    A[3, 2] = v * td / P.L
+    A_lin = np.eye(P.N) + P.DT * A
+
+    B = np.zeros((P.N, P.M))
+    B[2, 0] = 1
+    B[3, 1] = v / (P.L * cd**2)
+    B_lin = P.DT * B
+
+    f_xu = np.array([v * ct, v * st, a, v * td / P.L]).reshape(P.N, 1)
+    C_lin = (
+        P.DT
+        * (
+            f_xu - np.dot(A, x_bar.reshape(P.N, 1)) - np.dot(B, u_bar.reshape(P.M, 1))
+        ).flatten()
+    )
+
+    # return np.round(A_lin,6), np.round(B_lin,6), np.round(C_lin,6)
+    return A_lin, B_lin, C_lin
