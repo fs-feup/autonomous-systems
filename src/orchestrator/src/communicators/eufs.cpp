40289a5bb2839b778964e4ae3111787c9858dca2
Joao Romao <digasromao2000@gmail.com>
2023-06-29 20:44:05 +0100

    Orchestrating State and Odom calls

--- src/orchestrator/src/communicators/eufs.cpp
+++ src/orchestrator/src/communicators/eufs.cpp
@@ -6,19 +6,23 @@
 #include <functional>
 
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
-#include "custom_interfaces/msg/vcu_command.hpp"
+#include "custom_interfaces/msg/odom.hpp"
 #include "custom_interfaces/msg/vcu.hpp"
+#include "custom_interfaces/msg/vcu_command.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
 
-EufsCommunicator::EufsCommunicator(Orchestrator* orchestrator)
-    : Communicator() {
+EufsCommunicator::EufsCommunicator(Orchestrator* orchestrator) : Communicator() {
   this->orchestrator_ = orchestrator;
   this->sim_publisher_ =
       this->orchestrator_->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("/cmd", 10);
-  this->sim_subscriber_ = this->orchestrator_->create_subscription<nav_msgs::msg::Odometry>(
-      "/ground_truth/odom", 10,
-      std::bind(&EufsCommunicator::send_from_car, this, std::placeholders::_1));
+  this->state_subscriber_ = this->orchestrator_->create_subscription<custom_interfaces::msg::State>(
+      "/ros_can/state", 10,
+      std::bind(&EufsCommunicator::send_state_from_car, this, std::placeholders::_1));
+  this->odom_subscriber_ =
+      this->orchestrator_->create_subscription<custom_interfaces::msg::Odom>(
+          "/ros_can/wheel_speeds", 10,
+          std::bind(&EufsCommunicator::send_odom_from_car, this, std::placeholders::_1));
 }
 
 void EufsCommunicator::send_to_car(const custom_interfaces::msg::VcuCommand msg) {
@@ -28,7 +32,10 @@ void EufsCommunicator::send_to_car(const custom_interfaces::msg::VcuCommand msg)
   this->sim_publisher_->publish(command);
 }
 
-void EufsCommunicator::send_from_car(const nav_msgs::msg::Odometry msg) {
-  custom_interfaces::msg::Vcu res;
-  this->orchestrator_->publish_vcu(res);
+void EufsCommunicator::send_state_from_car(const custom_interfaces::msg::State msg) {
+  this->orchestrator_->publish_state(msg);
+}
+
+void EufsCommunicator::send_odom_from_car(const custom_interfaces::msg::Odom msg) {
+  this->orchestrator_->publish_odom(msg);
 }
