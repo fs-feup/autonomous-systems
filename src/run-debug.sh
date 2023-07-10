2ba5238bef6ed7c950fc6f1a59b25a6c36f3512a
marhcouto <marhcouto1101@gmail.com>
2023-07-10 15:37:51 +0100

    feat: plots adjusted and preliminary metrics calculation

--- src/run-debug.sh
+++ src/run-debug.sh
@@ -1,5 +1,7 @@
 colcon build --symlink-install
 
+source install/setup.bash
+
 # Perception
 ros2 launch yolov5_ros yolov5s_simple.launch.py&
 ros2 run depth_processing depth_processing_node&
--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,7 @@
 colcon build --symlink-install
 
+source install/setup.bash
+
 # Perception
 ros2 launch yolov5_ros yolov5s_simple.launch.py& --ros-args --log-level warn
 ros2 run depth_processing depth_processing_node& --ros-args --log-level warn
