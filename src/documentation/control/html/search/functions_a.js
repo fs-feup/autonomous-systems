10cca985dbd1111f607b1552ab7255e7fc400595
marhcouto <marhcouto1101@gmail.com>
2023-07-11 14:08:03 +0100

    fix(loc_map): fixed observation model

--- src/documentation/control/html/search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['timer_5fcallback_0',['timer_callback',['../classcontrol_1_1abstraction__layer_1_1AbstractionLayer.html#a9692d7a212fa89bc61dacc687e826097',1,'control.abstraction_layer.AbstractionLayer.timer_callback()'],['../classcontrol_1_1abstraction__layer_1_1SimInterface.html#a9692d7a212fa89bc61dacc687e826097',1,'control.abstraction_layer.SimInterface.timer_callback()'],['../classcontrol_1_1abstraction__layer_1_1CANInterface.html#a9692d7a212fa89bc61dacc687e826097',1,'control.abstraction_layer.CANInterface.timer_callback()']]]
-];
--- src/documentation/control/html/search/variables_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['t_0',['T',['../classcontrol_1_1config_1_1Params.html#adf1f3edb9115acb0a1e04209b7a9937b',1,'control::config::Params']]]
-];
