24e134acdd9c1a36b5d828a24ecea7214244c586
João Romão <digasromao2000@gmail.com>
2023-07-12 15:54:22 +0100

    feat(planning): mission control in fsds

--- src/planning/planning/include/adapter/adapter.hpp
+++ src/planning/planning/include/adapter/adapter.hpp
@@ -3,8 +3,10 @@
 
 #include <string>
 
-#include "eufs_msgs/srv/set_can_state.hpp"
 #include "eufs_msgs/msg/can_state.hpp"
+#include "eufs_msgs/srv/set_can_state.hpp"
+#include "fs_msgs/msg/finished_signal.hpp"
+#include "fs_msgs/msg/go_signal.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class Planning;
@@ -20,6 +22,7 @@ class Adapter {
   void ads_dv_init();
 
   void eufs_mission_state_callback(eufs_msgs::msg::CanState msg);
+  void fsds_mission_state_callback(const fs_msgs::msg::GoSignal);
   void eufs_set_mission_state(int mission, int state);
 
  public:
