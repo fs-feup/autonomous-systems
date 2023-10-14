0a410c9e5f994f53dd1f8f69d584026d6820698f
Davide64-dev <davidpcu@hotmail.com>
2023-10-14 16:15:01 +0100

    test(perception): implemented tests for yolo, depth processor and both

---
+++ src/perception/test/test_depthProcessor.py
@@ -0,0 +1,63 @@
+import cv2 as cv
+import os
+original_imread = cv.imread # It is necessary to keep this function. It will be overrided 
+import unittest
+import rclpy
+from rclpy.node import Node
+from custom_interfaces.msg import ConeArray
+from perception.main import perception
+import numpy as np
+import base64
+import time
+
+
+class TestPerceptionNode(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init(args=None)
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.node = perception()
+
+    def test_depthProcessor(self):
+
+        file = open('test/output/output.csv', 'a')
+
+        if (os.stat("test/output/output.csv").st_size == 0):
+            file.write("Module,Scope,Scenario,Execution Time (ms)\n")
+
+        dir = os.path.dirname(os.path.abspath(__file__))
+
+        for filename in os.listdir(dir + '/images'):
+            image = original_imread(dir + '/images/' + filename)
+
+            class_list, confidence_list,\
+            x_min_list, y_min_list, x_max_list, y_max_list =\
+            self.node.yolov5.image_callback(image)
+
+            msg = self.node.yolovFive2bboxes_msgs(bboxes=[x_min_list, y_min_list, 
+                                                 x_max_list, y_max_list], 
+                                                 scores=confidence_list, 
+                                                 cls=class_list)
+            sumTime = 0.0
+            n_times = 10
+            for i in range (n_times):
+                start_time = time.time()
+                self.node.depth_processor.process(msg, image, None)
+                end_time = time.time()
+                sumTime += (end_time - start_time) * 1000
+
+            averageTime = sumTime / n_times
+
+            file.write(f"Perception,Depth Processor,{filename},{averageTime}\n")
+
+        file.close()
+        self.node.destroy_node()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/perception/test/test_yolo.py
@@ -0,0 +1,55 @@
+import cv2 as cv
+import os
+original_imread = cv.imread # It is necessary to keep this function. It will be overrided 
+import unittest
+import rclpy
+from rclpy.node import Node
+from custom_interfaces.msg import ConeArray
+from perception.main import perception
+import numpy as np
+import base64
+import time
+
+
+class TestPerceptionNode(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init(args=None)
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.node = perception()
+
+    def test_yolo(self):
+
+        file = open('test/output/output.csv', 'a')
+        
+        if (os.stat("test/output/output.csv").st_size == 0):
+            file.write("Module,Scope,Scenario,Execution Time (ms)\n")
+
+        dir = os.path.dirname(os.path.abspath(__file__))
+
+        for filename in os.listdir(dir + '/images'):
+            image = original_imread(dir + '/images/' + filename)
+
+            sumTime = 0.0
+            n_times = 10
+            for i in range (n_times):
+                start_time = time.time()
+                self.node.yolov5.image_callback(image)
+                end_time = time.time()
+                sumTime += (end_time - start_time) * 1000
+
+            averageTime = sumTime / n_times
+
+            file.write(f"Perception,Yolov5,{filename},{averageTime}\n")
+
+        file.close()
+        self.node.destroy_node()
+
+if __name__ == '__main__':
+    unittest.main()
