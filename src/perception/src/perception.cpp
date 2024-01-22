400901872934addc02e24a33c4a7dda5fb033f67
marhcouto <marhcouto1101@gmail.com>
2024-01-22 13:59:26 +0000

    fix(perception): fixed linting issues

--- src/perception/src/perception.cpp
+++ src/perception/src/perception.cpp
@@ -1,26 +1,18 @@
 #include "perception/perception.hpp"
+
 #include <cstdio>
 
-
-Perception::Perception() : Node("perception"){
-
+Perception::Perception() : Node("perception") {
   this->_point_cloud_subscription = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-    "/lidar/point_cloud",
-    10,
-    std::bind(&Perception::pointCloudCallback, this, std::placeholders::_1)
-  );
+      "/lidar/point_cloud", 10,
+      std::bind(&Perception::pointCloudCallback, this, std::placeholders::_1));
 
-  this->_cones_publisher = this->create_publisher<custom_interfaces::msg::ConeArray>(
-    "cones",
-    10
-  );
-
+  this->_cones_publisher = this->create_publisher<custom_interfaces::msg::ConeArray>("cones", 10);
 }
 
-void Perception::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg){
-  // TODO
+void Perception::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+  // TODO(davide): do this
 }
-
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
