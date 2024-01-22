7439f0c306d6be9a458798260466524078121da2
marhcouto <marhcouto1101@gmail.com>
2024-01-22 13:59:26 +0000

    fix(perception): fixed linting issues

--- src/perception/include/perception/perception.hpp
+++ src/perception/include/perception/perception.hpp
@@ -1,16 +1,13 @@
-#include "rclcpp/rclcpp.hpp"
 #include "custom_interfaces/msg/cone_array.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
-
 class Perception : public rclcpp::Node {
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _point_cloud_subscription;
+  rclcpp::Publisher<custom_interfaces::msg::ConeArray>::SharedPtr _cones_publisher;
 
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _point_cloud_subscription;
-    rclcpp::Publisher<custom_interfaces::msg::ConeArray>::SharedPtr _cones_publisher;
+  void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
-    void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-
-    public:
-        Perception();
-
+ public:
+  Perception();
 };
