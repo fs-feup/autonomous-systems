286018038266757c0e4bb5454747cdfe3f35d9b7
lourenco31 <r.lourenco.a@gmail.com>
2024-01-31 15:15:22 +0000

    Removed unnecessary variables

--- src/long_control/include/node_/node_long_control.hpp
+++ src/long_control/include/node_/node_long_control.hpp
@@ -7,23 +7,18 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
+class LongitudinalControl : public rclcpp::Node {                           //class that holds fucntions to publish and hold data
+    private:
+        //declaration of 4 variables
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr result;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr current_velcoity;
+        rclcpp::Subscription<custom_interfaces::msg::ConeArray>::SharedPtr path_subscription;
+        double velocity;
 
-class LongitudinalControl : public rclcpp::Node {
- private:
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr
-      result;  // variable previously named publisher_
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr
-      current_velcoity;  // variable previously named subscriber_
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr desired_velcoity;
-  rclcpp::Subscription<custom_interfaces::msg::ConeArray>::SharedPtr path_subscription;
-  double velocity;
+        void publish_torque(custom_interfaces::msg::ConeArray path);              // function that only publishes torque (or other output such as acceleration)
 
-  void publish_torque(custom_interfaces::msg::ConeArray path);
+        void velocity_estimation_callback(std_msgs::msg::String velocity);        //function to transfer data to the variable "velocity"
 
-  void velocity_estimation_callback(std_msgs::msg::String velocity);
-
- public:
-  LongitudinalControl();
+    public:
+        LongitudinalControl();
 };
--- src/long_control/src/node_/node_long_control.cpp
+++ src/long_control/src/node_/node_long_control.cpp
@@ -9,27 +9,23 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
+// This example creates a subclass of Node and uses std::bind() to define which function gets executed at each time
 
-void LongitudinalControl::publish_torque(
-    custom_interfaces::msg::ConeArray path) {
+void LongitudinalControl::publish_torque(custom_interfaces::msg::ConeArray path) {                //function that publishes data whenever a new path is obtained
   auto torque = std_msgs::msg::String();
 
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", torque.data.c_str());
-  result->publish(torque);
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", torque.data.c_str());       
+  result->publish(torque);                                                                        //publish torque
 }
-void LongitudinalControl::velocity_estimation_callback(std_msgs::msg::String velocity) {}
+void LongitudinalControl::velocity_estimation_callback(std_msgs::msg::String velocity) {}         //function to transfer the velocity data to a variable
 
-LongitudinalControl::LongitudinalControl() : Node("node_long_control") {
-  current_velcoity = this->create_subscription<std_msgs::msg::String>(
-      "velocity_estimation", 10,
-      std::bind(&LongitudinalControl::velocity_estimation_callback, this,
-                std::placeholders::_1));  // get data from state estimation
-  path_subscription = this->create_subscription<custom_interfaces::msg::ConeArray>(
-      "local_planning", 10,
-      std::bind(&LongitudinalControl::publish_torque, this, std::placeholders::_1));
+LongitudinalControl::LongitudinalControl() : Node("node_long_control") {                                //constructor
 
-  result = this->create_publisher<std_msgs::msg::String>(
-      "torque_topic", 10);  // changed the name of topic from "topic" to "torque_topic"
+  current_velcoity = this->create_subscription<std_msgs::msg::String>("velocity_estimation",            // get velocity data from state estimation
+  10, std::bind(&LongitudinalControl::velocity_estimation_callback, this, std::placeholders::_1));      
+
+  path_subscription = this->create_subscription<custom_interfaces::msg::ConeArray>("local_planning",    // get path (and ideal velocity associated) form planning
+  10, std::bind(&LongitudinalControl::publish_torque, this, std::placeholders::_1));                    
+
+  result = this->create_publisher<std_msgs::msg::String>("torque_topic", 10);                           //creates publisher that should yield torque/acceleration/...
 }
