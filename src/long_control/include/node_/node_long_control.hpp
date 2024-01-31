eab7b92caaae28e12958d751af41815a00b33f93
lourenco31 <r.lourenco.a@gmail.com>
2024-01-31 15:37:09 +0000

    Removed unnecessary variable from hpp file and added comments

--- src/long_control/include/node_/node_long_control.hpp
+++ src/long_control/include/node_/node_long_control.hpp
@@ -7,18 +7,18 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-class LongitudinalControl : public rclcpp::Node {                           //class that holds fucntions to publish and hold data
-    private:
+//class that holds fucntions to publish and hold data
+class LongitudinalControl : public rclcpp::Node{
+ private:
         //declaration of 4 variables
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr result;
         rclcpp::Subscription<std_msgs::msg::String>::SharedPtr current_velcoity;
         rclcpp::Subscription<custom_interfaces::msg::ConeArray>::SharedPtr path_subscription;
         double velocity;
-
-        void publish_torque(custom_interfaces::msg::ConeArray path);              // function that only publishes torque (or other output such as acceleration)
-
-        void velocity_estimation_callback(std_msgs::msg::String velocity);        //function to transfer data to the variable "velocity"
-
-    public:
+        // function that only publishes torque (or other output such as acceleration)
+        void publish_torque(custom_interfaces::msg::ConeArray path);
+        //function to transfer data to the variable "velocity"
+        void velocity_estimation_callback(std_msgs::msg::String velocity);
+ public:
         LongitudinalControl();
 };
