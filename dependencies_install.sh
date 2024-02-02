bbb8ec977b0a9ed2af901fcb46573b23931c597d
Davide64-dev <davidpcu@hotmail.com>
2024-02-02 15:30:23 +0000

    feat: PCL integraiton

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -3,3 +3,9 @@ sudo apt install ros-humble-ackermann-msgs
 # planning
 sudo apt-get install libcgal-dev
 sudo apt-get install libgsl-dev
+
+# perception
+
+sudo apt install libpcl-dev
+sudo apt install ros-galactic-rtabmap-ros
+sudo apt install ros-galactic-rtabmap
--- src/perception/include/perception/perception_node.hpp
+++ src/perception/include/perception/perception_node.hpp
@@ -1,13 +1,17 @@
 #include "custom_interfaces/msg/cone_array.hpp"
+#include "ground_removal/ransac.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
 class Perception : public rclcpp::Node {
+
+private:
+  GroundRemoval* groundRemoval;
+
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _point_cloud_subscription;
   rclcpp::Publisher<custom_interfaces::msg::ConeArray>::SharedPtr _cones_publisher;
-
-  void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+  void pointCloudCallback(const sensor_msgs::msg::PointCloud2 msg);
 
  public:
-  Perception();
+  Perception(GroundRemoval* groundRemoval);
 };
