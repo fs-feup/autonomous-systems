40da321e6795a7ebfff5dee3e666ea0045eb61d3
Davide64-dev <davidpcu@hotmail.com>
2024-02-03 18:24:28 +0000

    feat(perception): RANSAC is implemented

--- src/perception/include/ground_removal/ground_removal.hpp
+++ src/perception/include/ground_removal/ground_removal.hpp
@@ -1,7 +1,11 @@
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include <pcl/PCLPointField.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 class GroundRemoval {
 
     public:
-        virtual sensor_msgs::msg::PointCloud2 groundRemoval(sensor_msgs::msg::PointCloud2 point_cloud) const = 0;
+        virtual void groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud, pcl::PointCloud<pcl::PointXYZI> &ret) const = 0;
 };
--- src/perception/src/ground_removal/ransac.cpp
+++ src/perception/src/ground_removal/ransac.cpp
@@ -1,13 +1,37 @@
 #include "ground_removal/ransac.hpp"
-#include <pcl/common/io.h>
-#include <pcl/conversions.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
 
 RANSAC::RANSAC(double epsilon, int n_tries) : epsilon(epsilon), n_tries(n_tries) {}
 
 
-sensor_msgs::msg::PointCloud2 RANSAC::groundRemoval(sensor_msgs::msg::PointCloud2 point_cloud) const{
+void RANSAC::groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud, pcl::PointCloud<pcl::PointXYZI> &ret) const{
+
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers_indices (new pcl::PointIndices);
+
+    pcl::SACSegmentation<pcl::PointXYZI> seg;
+
+    seg.setOptimizeCoefficients (true);
+
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setDistanceThreshold (epsilon);
+    seg.setMaxIterations(n_tries);
 
 
-    
-    return point_cloud;
+    seg.setInputCloud (point_cloud);
+    seg.segment (*inliers_indices, *coefficients);
+
+    pcl::ExtractIndices<pcl::PointXYZI> extract;
+    extract.setInputCloud(point_cloud);
+    extract.setIndices(inliers_indices);
+
+    extract.setNegative(true); // Extract outliers
+    extract.filter(ret);
 }
