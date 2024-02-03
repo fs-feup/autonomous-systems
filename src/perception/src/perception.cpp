f2573a9ea32ef8f2f5cc3edef01f8cbdce82360b
Davide64-dev <davidpcu@hotmail.com>
2024-02-03 18:24:28 +0000

    feat(perception): RANSAC is implemented

--- src/perception/src/perception.cpp
+++ src/perception/src/perception.cpp
@@ -7,14 +7,9 @@
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
 
-  auto ground_removal = new RANSAC(1.1, 5);
+  auto ground_removal = new RANSAC(0.1, 1000);
 
   auto node = std::make_shared<Perception>(ground_removal);
-
-   pcl::PointCloud<pcl::PointXYZ>::Ptr pclCloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pclCloud->width = 5;
-    pclCloud->height = 1;
-    pclCloud->points.resize(pclCloud->width * pclCloud->height);
 
   RCLCPP_INFO(node->get_logger(), "Perception is alive!");
 
