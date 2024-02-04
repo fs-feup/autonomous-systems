42385d0d9961cc9cc861ad3217190d2be7faa834
Davide64-dev <davidpcu@hotmail.com>
2024-02-04 00:06:01 +0000

    test(perception): Created RANSAC tests

--- src/perception/include/perception/perception_node.hpp
+++ src/perception/include/perception/perception_node.hpp
@@ -3,15 +3,34 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
+
+/**
+ * @class Perception
+ * @brief Node for perception tasks, such as ground removal and cone detection.
+ *
+ * This class is a ROS 2 node that subscribes to a PointCloud2 topic, performs
+ * ground removal using the specified GroundRemoval algorithm, and publishes
+ * cone detection results on a custom topic.
+ */
 class Perception : public rclcpp::Node {
 
 private:
-  GroundRemoval* groundRemoval;
+  GroundRemoval* groundRemoval; ///< Pointer to the GroundRemoval object.
 
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _point_cloud_subscription;
-  rclcpp::Publisher<custom_interfaces::msg::ConeArray>::SharedPtr _cones_publisher;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _point_cloud_subscription;  ///< PointCloud2 subscription.
+  rclcpp::Publisher<custom_interfaces::msg::ConeArray>::SharedPtr _cones_publisher; ///< ConeArray publisher.
+  
+  /**
+   * @brief Callback function for the PointCloud2 subscription.
+   * @param msg The received PointCloud2 message.
+   */
   void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
  public:
+
+   /**
+   * @brief Constructor for the Perception node.
+   * @param groundRemoval Pointer to the GroundRemoval object.
+   */
   Perception(GroundRemoval* groundRemoval);
 };
