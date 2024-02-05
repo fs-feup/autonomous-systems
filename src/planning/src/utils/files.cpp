7e09867337e67de959329d378a9aaca8563b2d13
lourenco31 <r.lourenco.a@gmail.com>
2024-02-05 23:03:38 +0000

    fix (planning): fixed tests and resolved cpplint issues

--- src/planning/src/utils/files.cpp
+++ src/planning/src/utils/files.cpp
@@ -22,24 +22,24 @@ std::vector<PathPoint *> read_path_file(const std::string &filename) {
 std::ofstream openWriteFile(const std::string &filename) {
   std::string filePrefix = ament_index_cpp::get_package_share_directory("planning");
   filePrefix = filePrefix.substr(0, filePrefix.find("install"));
-  std::string logger_variable= filePrefix + filename;
+  std::string logger_variable = filePrefix + filename;
   std::ofstream file(filePrefix + filename, std::ios::app);
   if (!file.is_open()) {
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "ERROR opening file: %s\n", logger_variable.c_str());
-  }
-  else {RCLCPP_DEBUG("Successfully opened %s \n", logger_variable.c_str());}
+  } else {RCLCPP_DEBUG(rclcpp::get_logger("rclcpp"),
+   "Successfully opened %s \n", logger_variable.c_str());}
   return file;
 }
 
 std::ifstream openReadFile(const std::string &filename) {
   std::string filePrefix = ament_index_cpp::get_package_share_directory("planning");
   filePrefix = filePrefix.substr(0, filePrefix.find("install"));
-  std::string logger_variable= filePrefix + filename;
+  std::string logger_variable = filePrefix + filename;
   std::ifstream file(filePrefix + filename);
   if (!file.is_open()) {
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "ERROR opening file: %s\n", logger_variable.c_str());
-  }
-  else {RCLCPP_DEBUG("Successfully opened %s \n", logger_variable.c_str());}
+  } else {RCLCPP_DEBUG(rclcpp::get_logger("rclcpp"),
+   "Successfully opened %s \n", logger_variable.c_str());}
   return file;
 }
 // Track* read_track_file(const std::string& filename) {
