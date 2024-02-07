3095aad924bda23ddb93576a576b635c4eeed1a3
Davide64-dev <davidpcu@hotmail.com>
2024-02-07 15:18:38 +0000

    fix(perception): added some dependencies

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -3,3 +3,10 @@ sudo apt install ros-humble-ackermann-msgs
 # planning
 sudo apt-get install libcgal-dev
 sudo apt-get install libgsl-dev
+
+
+# perception
+sudo apt-get install -y ros-sensor-msgs
+sudo apt install libsensor-msgs-dev
+sudo apt install libpcl-conversions-dev
+sudo apt-get install ros-humble-pcl-conversions
