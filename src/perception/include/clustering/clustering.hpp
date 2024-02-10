fd7bdf056b7f36831bb09fe76a0ea3561973b098
Davide64-dev <davidpcu@hotmail.com>
2024-02-10 19:53:55 +0000

    feat(perception): a first iteration of the algorithm is done

--- src/perception/include/clustering/clustering.hpp
+++ src/perception/include/clustering/clustering.hpp
@@ -2,10 +2,11 @@
 #include <pcl/PCLPointField.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <vector>
 
 class Clustering {
  public:
 
         virtual void clustering(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud,
-                                   vector<pcl::PointCloud<pcl::PointXYZI>> ret) const = 0;
+                           std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr>& clusters) const = 0;
 };
