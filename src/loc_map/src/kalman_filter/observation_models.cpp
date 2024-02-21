2bc4c35221602029ae04b99b2c27343fff21fd48
Pedro Romao <up202108660@g.uporto.pt>
2024-02-21 18:27:33 +0000

    cleans up code

--- src/loc_map/src/kalman_filter/observation_models.cpp
+++ src/loc_map/src/kalman_filter/observation_models.cpp
@@ -41,6 +41,8 @@ Eigen::Vector2f ObservationModel::observation_model(const Eigen::VectorXf &expec
 Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
     const Eigen::VectorXf &expected_state, const unsigned int landmark_index,
     const unsigned int state_size) const {
+
+  // //direct insertion
   // Eigen::MatrixXf validation_jacobian = Eigen::MatrixXf::Zero(2, state_size);
 
   // validation_jacobian(0, 0) = -cos(-expected_state(2));
@@ -60,11 +62,12 @@ Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
   // validation_jacobian(0, landmark_index + 1) = -sin(-expected_state(2));
   // validation_jacobian(1, landmark_index + 1) = cos(-expected_state(2));
 
-  // //test with blocks
+
+
+  // // test with blocks
   // Eigen::MatrixXf block_1 = Eigen::MatrixXf::Identity(3, 3);
   // Eigen::MatrixXf block_2 = Eigen::MatrixXf::Identity(2, 2);
 
-  // //test with sub low_jacobian
   // Eigen::MatrixXf low_jacobian_1 = Eigen::MatrixXf::Zero(2, 3);
   // Eigen::MatrixXf low_jacobian_2 = Eigen::MatrixXf::Zero(2, 2);
 
@@ -73,13 +76,13 @@ Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
   // low_jacobian_1(0, 1) = sin(-expected_state(2));
   // low_jacobian_1(1, 1) = -cos(-expected_state(2));
   // low_jacobian_1(0, 2) = -expected_state(landmark_index) * sin(expected_state(2)) +
-  //                      expected_state(landmark_index + 1) * cos(-expected_state(2)) +
-  //                      expected_state(0) * sin(expected_state(2)) -
-  //                      expected_state(1) * cos(-expected_state(2));
+  //                        expected_state(landmark_index + 1) * cos(-expected_state(2)) +
+  //                        expected_state(0) * sin(expected_state(2)) -
+  //                        expected_state(1) * cos(-expected_state(2));
   // low_jacobian_1(1, 2) = -expected_state(landmark_index) * cos(-expected_state(2)) -
-  //                      expected_state(landmark_index + 1) * sin(expected_state(2)) +
-  //                      expected_state(0) * cos(-expected_state(2)) +
-  //                      expected_state(1) * sin(expected_state(2));
+  //                        expected_state(landmark_index + 1) * sin(expected_state(2)) +
+  //                        expected_state(0) * cos(-expected_state(2)) +
+  //                        expected_state(1) * sin(expected_state(2));
 
   // low_jacobian_2(0, 0) = cos(-expected_state(2));
   // low_jacobian_2(1, 0) = sin(-expected_state(2));
@@ -94,37 +97,35 @@ Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
   // validation_jacobian.block(0, 0, 2, 3) = validation_jacobian_1;
   // validation_jacobian.block(0, landmark_index, 2, 2) = validation_jacobian_2;
 
-  
-    Eigen::MatrixXf reformating_matrix = Eigen::MatrixXf::Zero(5, state_size);
-    reformating_matrix(0, 0) = 1;
-    reformating_matrix(1, 1) = 1;
-    reformating_matrix(2, 2) = 1;
-    reformating_matrix(3, landmark_index) = 1;
-    reformating_matrix(4, landmark_index + 1) = 1;
+  // old imlementation
+  Eigen::MatrixXf reformating_matrix = Eigen::MatrixXf::Zero(5, state_size);
+  reformating_matrix(0, 0) = 1;
+  reformating_matrix(1, 1) = 1;
+  reformating_matrix(2, 2) = 1;
+  reformating_matrix(3, landmark_index) = 1;
+  reformating_matrix(4, landmark_index + 1) = 1;
 
-    Eigen::MatrixXf low_jacobian = Eigen::MatrixXf::Zero(2, 5);
-    // partial derivative of h(x) or z_hat (from observation model) with respect to
-    // (x,y,theta,landmark_x,landmark_y)
-    low_jacobian(0, 0) = -cos(-expected_state(2));
-    low_jacobian(1, 0) = -sin(-expected_state(2));
-    low_jacobian(0, 1) = sin(-expected_state(2));
-    low_jacobian(1, 1) = -cos(-expected_state(2));
-    low_jacobian(0, 2) = -expected_state(landmark_index) * sin(expected_state(2)) +
-                         expected_state(landmark_index + 1) * cos(-expected_state(2)) +
-                         expected_state(0) * sin(expected_state(2)) -
-                         expected_state(1) * cos(-expected_state(2));
-    low_jacobian(1, 2) = -expected_state(landmark_index) * cos(-expected_state(2)) -
-                         expected_state(landmark_index + 1) * sin(expected_state(2)) +
-                         expected_state(0) * cos(-expected_state(2)) +
-                         expected_state(1) * sin(expected_state(2));
-    low_jacobian(0, 3) = cos(-expected_state(2));
-    low_jacobian(1, 3) = sin(-expected_state(2));
-    low_jacobian(0, 4) = -sin(-expected_state(2));
-    low_jacobian(1, 4) = cos(-expected_state(2));
+  Eigen::MatrixXf low_jacobian = Eigen::MatrixXf::Zero(2, 5);
+  // partial derivative of h(x) or z_hat (from observation model) with respect to
+  // (x,y,theta,landmark_x,landmark_y)
+  low_jacobian(0, 0) = -cos(-expected_state(2));
+  low_jacobian(1, 0) = -sin(-expected_state(2));
+  low_jacobian(0, 1) = sin(-expected_state(2));
+  low_jacobian(1, 1) = -cos(-expected_state(2));
+  low_jacobian(0, 2) = -expected_state(landmark_index) * sin(expected_state(2)) +
+                       expected_state(landmark_index + 1) * cos(-expected_state(2)) +
+                       expected_state(0) * sin(expected_state(2)) -
+                       expected_state(1) * cos(-expected_state(2));
+  low_jacobian(1, 2) = -expected_state(landmark_index) * cos(-expected_state(2)) -
+                       expected_state(landmark_index + 1) * sin(expected_state(2)) +
+                       expected_state(0) * cos(-expected_state(2)) +
+                       expected_state(1) * sin(expected_state(2));
+  low_jacobian(0, 3) = cos(-expected_state(2));
+  low_jacobian(1, 3) = sin(-expected_state(2));
+  low_jacobian(0, 4) = -sin(-expected_state(2));
+  low_jacobian(1, 4) = cos(-expected_state(2));
+  Eigen::MatrixXf validation_jacobian = low_jacobian * reformating_matrix;
 
-    Eigen::MatrixXf validation_jacobian = low_jacobian * reformating_matrix;
-
-    
   return validation_jacobian;
 }
 
