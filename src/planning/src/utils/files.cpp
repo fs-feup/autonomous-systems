ede3a318ec19ae0108a17068813f29d970956f79
lourenco31 <r.lourenco.a@gmail.com>
2024-02-25 18:45:27 +0000

    fix(planning): removed unnecessary function

--- src/planning/src/utils/files.cpp
+++ src/planning/src/utils/files.cpp
@@ -45,19 +45,6 @@ std::ifstream openReadFile(const std::string &filename) {
   return file;
 }
 
-void deleteContentsInFile(const std::string &filename) {
-  std::string filePrefix = ament_index_cpp::get_package_share_directory("planning");
-  filePrefix = filePrefix.substr(0, filePrefix.find("install"));
-  std::string logger_variable = filePrefix + filename;
-  std::ofstream ofs;
-  ofs.open(logger_variable, std::ofstream::out | std::ofstream::trunc);
-  if (!ofs.is_open()) {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
-    "ERROR cleaning file: %s\n", logger_variable.c_str());
-  } else {RCLCPP_DEBUG(rclcpp::get_logger("rclcpp"),
-   "Successfully cleaned file:  %s \n", logger_variable.c_str());}
-  ofs.close();
-}
 
 // Track* read_track_file(const std::string& filename) {
 //   std::string filePrefix = rcpputils::fs::current_path().string();
