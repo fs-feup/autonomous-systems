351230682bffbd05a106b2566032fd171a6c1a95
Pedro Romao <up202108660@g.uporto.pt>
2024-02-28 17:25:07 +0000

    comment explaining low jacobian calculation

--- src/loc_map/src/kalman_filter/observation_models.cpp
+++ src/loc_map/src/kalman_filter/observation_models.cpp
@@ -41,7 +41,6 @@ Eigen::Vector2f ObservationModel::observation_model(const Eigen::VectorXf &expec
 Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
     const Eigen::VectorXf &expected_state, const unsigned int landmark_index,
     const unsigned int state_size) const {
-
   // //direct insertion
   // Eigen::MatrixXf validation_jacobian = Eigen::MatrixXf::Zero(2, state_size);
 
@@ -61,8 +60,6 @@ Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
   // validation_jacobian(1, landmark_index) = sin(-expected_state(2));
   // validation_jacobian(0, landmark_index + 1) = -sin(-expected_state(2));
   // validation_jacobian(1, landmark_index + 1) = cos(-expected_state(2));
-
-
 
   // // test with blocks
   // Eigen::MatrixXf block_1 = Eigen::MatrixXf::Identity(3, 3);
@@ -105,9 +102,20 @@ Eigen::MatrixXf ObservationModel::get_state_to_observation_matrix(
   reformating_matrix(3, landmark_index) = 1;
   reformating_matrix(4, landmark_index + 1) = 1;
 
-  Eigen::MatrixXf low_jacobian = Eigen::MatrixXf::Zero(2, 5);
-  // partial derivative of h(x) or z_hat (from observation model) with respect to
+  // partial derivative of h(x) or z_hat with respect to
   // (x,y,theta,landmark_x,landmark_y)
+  Eigen::MatrixXf low_jacobian = Eigen::MatrixXf::Zero(2, 5);
+
+  // the function h that is derived is the observation model function which after the matrix
+  // multiplication becomes:
+  //  expected_landmark_x = I * cos(t) + m * sin(t) - x * cos(t) - y * sin(t); -> FUNC_X
+  //  expected_landmark_y = -I * sin(t) + m * cos(t) + x * sin(t) - y * cos(t); -> FUNC_Y
+  // and so the low_jacobian below is:
+  // FUNC_X partially derived in x, y, theta, m, n
+  // FUNC_Y partially derived in x, y, theta, m, n
+  // where x is the x coordinate of the robot, y is the y coordinate of the robot, theta is the
+  // orientation of the robot, m is the x coordinate of the landmark, n is the y coordinate of the
+  // landmark
   low_jacobian(0, 0) = -cos(-expected_state(2));
   low_jacobian(1, 0) = -sin(-expected_state(2));
   low_jacobian(0, 1) = sin(-expected_state(2));
