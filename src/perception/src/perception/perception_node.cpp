4cf86512b3cb9304fe722b5294eeaebcaffca14a
Davide Teixeira <114567030+Davide64-dev@users.noreply.github.com>
2024-03-31 17:30:16 +0100

    FSF-2752 FSF-2770 Cylinder Validation (#260)
    * feat(perception): cylinder validation first iteration finished
    
    * ref(perception: refactored cylinder validator
    
    * mend
    
    * fix(perception): fixed warnings
    
    * fix(perception): problem with linting
    
    * ref(perception): vars/comments refactoring
    
    * feat(perception): finished height validator
    
    * test(perception): created tests for cone validators
    
    * mend
    
    ---------
    
    Co-authored-by: marhcouto <marhcouto1101@gmail.com>

--- src/perception/src/perception/perception_node.cpp
+++ src/perception/src/perception/perception_node.cpp
@@ -1,5 +1,6 @@
 #include "perception/perception_node.hpp"
 
+#include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -10,12 +11,16 @@
 #include "adapter/fsds.hpp"
 #include "adapter/map.hpp"
 #include "adapter/testlidar.hpp"
+#include <vector>
+
 Perception::Perception(GroundRemoval* groundRemoval, Clustering* clustering,
-                       ConeDifferentiation* coneDifferentiator)
+                       ConeDifferentiation* coneDifferentiator,
+                       const std::vector<ConeValidator*>& coneValidators)
     : Node("perception"),
       groundRemoval(groundRemoval),
       clustering(clustering),
-      coneDifferentiator(coneDifferentiator) {
+      coneDifferentiator(coneDifferentiator),
+      coneValidators(coneValidators) {
   this->_cones_publisher = this->create_publisher<custom_interfaces::msg::ConeArray>("cones", 10);
 
   this->adapter = adapter_map[mode](this);
@@ -28,11 +33,22 @@ void Perception::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedP
 
   pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
 
-  groundRemoval->groundRemoval(pcl_cloud, ground_removed_cloud);
+  groundRemoval->groundRemoval(pcl_cloud, ground_removed_cloud, groundPlane);
 
   std::vector<Cluster> clusters;
 
   clustering->clustering(ground_removed_cloud, &clusters);
+
+  std::vector<Cluster> filtered_clusters;
+
+  for (auto cluster : clusters) {
+      if (std::all_of(coneValidators.begin(), coneValidators.end(), [&](const auto& validator) {
+          return validator->coneValidator(&cluster, groundPlane);
+      })) {
+          filtered_clusters.push_back(cluster);
+      }
+  }
+
 
   RCLCPP_DEBUG(this->get_logger(), "---------- Point Cloud Received ----------");
   RCLCPP_DEBUG(this->get_logger(), "Point Cloud Before Ground Removal: %ld points",
@@ -41,18 +57,20 @@ void Perception::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedP
                ground_removed_cloud->points.size());
   RCLCPP_DEBUG(this->get_logger(), "Point Cloud after Clustering: %ld clusters", clusters.size());
 
-  for (int i = 0; i < clusters.size(); i++) {
-    coneDifferentiator->coneDifferentiation(&clusters[i]);
-    std::string color = clusters[i].getColor();
+
+  for (int i = 0; i < filtered_clusters.size(); i++) {
+    coneDifferentiator->coneDifferentiation(&filtered_clusters[i]);
+    std::string color = filtered_clusters[i].getColor();
     RCLCPP_DEBUG(this->get_logger(), "Cone %d: %s", i, color.c_str());
   }
 
-  publishCones(&clusters);
+
+  publishCones(&filtered_clusters);
 }
 
 void Perception::publishCones(std::vector<Cluster>* cones) {
   auto message = custom_interfaces::msg::ConeArray();
-  for (int i = 0; i < cones->size(); i++) {
+  for (int i = 0; i < static_cast<int>(cones->size()); i++) {
     auto position = custom_interfaces::msg::Point2d();
     position.x = cones->at(i).getCentroid().x();
     position.y = cones->at(i).getCentroid().y();
---
+++ src/perception/test/height_validator_test.cpp
@@ -0,0 +1,55 @@
+#include <gtest/gtest.h>
+#include <utils/cluster.hpp>
+#include <cone_validator/height_validator.hpp>
+#include <utils/plane.hpp>
+#include <pcl/PCLPointField.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+/**
+ * @brief Test fixture for HeightValidator class.
+ */
+class HeightValidatorTest : public ::testing::Test {
+ protected:
+    /**
+     * @brief Set up function to initialize a plane.
+     */
+    void SetUp() override {
+        plane = Plane(1, 0, 0, 0);
+    }
+
+    Plane plane; ///< Plane object for testing.
+};
+
+/**
+ * @brief Test case to validate if the cone height is within the height threshold.
+ */
+TEST_F(HeightValidatorTest, ConeWithinHeightThreshold) {
+    HeightValidator validator = HeightValidator(0.375);
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    point_cloud->points.push_back(pcl::PointXYZI{0.3, 0.0, 0, 0});
+
+    Cluster conePointCloud = Cluster(point_cloud);
+
+    bool result = validator.coneValidator(&conePointCloud, plane);
+
+    ASSERT_TRUE(result);
+}
+
+/**
+ * @brief Test case to validate if the cone height exceeds the height threshold.
+ */
+TEST_F(HeightValidatorTest, ConeExceedsHeightThreshold) {
+    HeightValidator validator = HeightValidator(0.375);
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    point_cloud->points.push_back(pcl::PointXYZI{1.0, 0.0, 0, 0});
+
+    Cluster conePointCloud = Cluster(point_cloud);
+
+    bool result = validator.coneValidator(&conePointCloud, plane);
+
+    ASSERT_FALSE(result);
+}
+
