3a97812c8563eac69200623ec2946e9822fa5aa3
Davide64-dev <davidpcu@hotmail.com>
2024-04-11 11:02:40 +0100

    doc(perception): documented distance prediction

--- src/perception/include/cone_evaluator/cone_evaluator.hpp
+++ src/perception/include/cone_evaluator/cone_evaluator.hpp
@@ -3,8 +3,20 @@
 
 #include <utils/cluster.hpp>
 
+/**
+ * @class ConeEvaluator 
+ * 
+ * @brief Abstract class that represents an heurisic that evaluates the cluster as a cone
+ * 
+ */
 class ConeEvaluator {
  public:
+ /**
+  * @brief Perform the cluster evaluation
+  * 
+  * @param cluster Cluster to evaluate
+  * @return double Cluster's confidence &isin; [0,1]
+  */
   virtual double evaluateCluster(Cluster& cluster) const = 0;
 };
 
--- src/perception/include/cone_evaluator/distance_predict.hpp
+++ src/perception/include/cone_evaluator/distance_predict.hpp
@@ -4,12 +4,31 @@
 #include <utils/cluster.hpp>
 #include <cone_evaluator/cone_evaluator.hpp>
 
+/**
+ * @class DistancePredict
+ * 
+ * @brief Cone heurisitic evaluation based on distance
+ * 
+ */
 class DistancePredict : public ConeEvaluator {
  private:
-  double vertical_ang_res;
-  double horizontal_ang_res;
+  double vertical_ang_res; ///< LiDAR's vertical angular resolution (degrees)
+  double horizontal_ang_res; ///< LiDAR's horizontal angular resolution (degrees)
  public:
+  /**
+   * @brief Constructor of a Distance Predict
+   * 
+   * @param vertical_ang_res LiDAR's vertical angular resolution (degrees)
+   * @param horizontal_ang_res LiDAR's horizontal angular resolution (degrees)
+   */
   DistancePredict(double vertical_ang_res, double horizontal_ang_res);
+
+  /**
+   * @brief Perform the cluster evaluation based on distance
+   * 
+   * @param cluster Cluster to evaluate
+   * @return double Cluster's confidence &isin; [0,1]
+   */
   double evaluateCluster(Cluster& cluster) const override;
 };
 
