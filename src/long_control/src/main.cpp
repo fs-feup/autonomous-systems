648dbd31bc90cac7d51020c84d68014afb5d0bc3
PedromLBarros <78809970+PedromLBarros@users.noreply.github.com>
2024-04-19 16:41:29 +0100

    FSF-2597 - Ros node (#262)
    * feat (ros_can): finding lib problem
    
    * feat (ros_can): finding lib problem
    
    * fix(ros-can): fixed Cmake and created script for installation
    
    * feat(ros_can): CAN Interface-read from BUS & publish topics
    
    * fix(ros_can): all fixs related with using more complex ros msgs
    
    * feat(ros_can):syncing system for wheels RPM
    
    * feat: activated static analysis for ros can
    
    * .
    
    * prepare rebase
    
    * fix: merged
    
    * sync method update to ros msg filters + steering sensor bosch
    
    * fix: small erros + readme
    
    * fix static analysis
    
    * fix cppcheck and colcon build - ready for review
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: marhcouto <marhcouto1101@gmail.com>
    Co-authored-by: FSFEUP <formulastudentfeup@gmail.com>

--- src/long_control/src/main.cpp
+++ src/long_control/src/main.cpp
@@ -3,7 +3,7 @@
 #include "node_/node_long_control.hpp"
 #include "rclcpp/rclcpp.hpp"
 /**
- * @brief Main function for the Longitudinal Control node.
+ * @brief Main function for the Ros Can node.
  *
  * Initializes the ROS node, creates a Longitudinal Control object,
  * enters the ROS 2 event loop, and then shuts down the node when done (spin is
