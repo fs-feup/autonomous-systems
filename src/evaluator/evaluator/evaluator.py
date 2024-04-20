8ffe9b6406311ad5cda48728812e55941e8ba475
Davide64-dev <davidpcu@hotmail.com>
2024-04-20 15:40:19 +0100

    ref(sim_inf): made some refactoring to evaluator and adapter

--- src/evaluator/evaluator/evaluator.py
+++ src/evaluator/evaluator/evaluator.py
@@ -27,25 +27,25 @@ class Evaluator(Node):
     def compute_and_publish_perception(self):
 
         mean_difference = Float32()
-        mean_difference.data = self.get_average_difference()
+        mean_difference.data = self.get_average_difference(self.perception_output, self.perception_ground_truth)
 
         inter_cones_distance = Float32()
-        inter_cones_distance.data = self.get_inter_cones_distance()
+        inter_cones_distance.data = self.get_inter_cones_distance(self.perception_output)
 
         self.perception_mean_difference.publish(mean_difference)
 
-        self.perception_inter_cones_distance(inter_cones_distance)
+        self.perception_inter_cones_distance.publish(inter_cones_distance)
 
-        self.get_logger(f"First metric: {mean_difference.data}")
 
-    def get_average_difference(self):
+    @staticmethod
+    def get_average_difference(perception_output, perception_ground_truth):
         sum = 0
         count = 0
-        for perception_cone in self.perception_output:
+        for perception_cone in perception_output:
             
-            minDistance = np.linalg.norm(perception_cone - self.perception_ground_truth[0])
+            minDistance = np.linalg.norm(perception_cone - perception_ground_truth[0])
 
-            for ground_truth_cone in self.perception_ground_truth:
+            for ground_truth_cone in perception_ground_truth:
                 distance = np.linalg.norm(perception_cone - ground_truth_cone)
 
                 if distance < minDistance:
@@ -58,8 +58,9 @@ class Evaluator(Node):
 
         return average
     
-    def get_inter_cones_distance(self):
-        size = len(self.perception_output)
+    @staticmethod
+    def get_inter_cones_distance(perception_output):
+        size = len(perception_output)
         visited = set()
         total_distance = 0
         num_pairs = 0
@@ -70,7 +71,7 @@ class Evaluator(Node):
                 nearest_index = None
                 for j in range(size):
                     if j != i and j not in visited:
-                        distance = np.linalg.norm(self.perception_output[i] - self.perception_output[j])
+                        distance = np.linalg.norm(perception_output[i] - perception_output[j])
                         if distance < nearest_distance:
                             nearest_distance = distance
                             nearest_index = j
