a544d4add29abf45df9cc2ebd46237568bbfe119
lourenco31 <150086800+lourenco31@users.noreply.github.com>
2024-05-01 16:15:53 +0100

    FSF-3540 Inspection fix (#281)
    * fix(inspection): fixes and minor refactor
    
    * fix: removed line added automatically by vscode extension to .vscode
    
    * fix(inspection): default constructor for functions and test fixed
    
    * fix(inspection): maximum angle back to 30 degrees
    
    * fix(inspection): sonarcloud issues
    
    * sonarcloud issues
    
    * fix(inpection) : publishes emergency message at the end of ebs test
    
    * update submodule
    
    * fix(inspection): sonarcloud issue
    
    * update submodules and solved conflicts
    
    * feat(inspection) : throttle values are between -1 and 1
    
    * update submodules
    
    * fix(inspection) : sonarcloud issues
    
    * fix(inspection) : fixed 'intentionality' issues in tests
    
    * fix sonarcloud issues with 'new'
    
    * fix(inspection) : error in throttle conversion to adequate range
    
    * fix error caused by ros extension

--- src/inspection/src/inspection_functions.cpp
+++ src/inspection/src/inspection_functions.cpp
@@ -14,27 +14,38 @@ InspectionFunctions::InspectionFunctions(double max_angle, double turning_period
   current_goal_velocity = ideal_velocity;
 }
 
-double InspectionFunctions::rpm_to_velocity(double rpm) {
+InspectionFunctions::InspectionFunctions() = default;
+
+double InspectionFunctions::rpm_to_velocity(double rpm) const {
   double perimeter = 2 * M_PI * wheel_radius;
   return rpm * perimeter / 60.0;
 }
 
-double InspectionFunctions::calculate_throttle(double current_velocity) {
+double InspectionFunctions::calculate_throttle(double current_velocity) const {
   double error = current_goal_velocity - current_velocity;
   return gain * error;
 }
 
-double InspectionFunctions::calculate_steering(double time) {
+double InspectionFunctions::calculate_steering(double time) const {
   return sin((time * 2 * M_PI / turning_period)) * max_angle;
 }
 
+double InspectionFunctions::throttle_to_adequate_range(double throttle) const {
+  if (fabs(throttle) >= MAX_THROTTLE) {
+    return (throttle > 0) ? 1 : -1;
+  }
+  return throttle/MAX_THROTTLE;
+}
+
 void InspectionFunctions::redefine_goal_velocity(double current_velocity) {
-  double velocity_error = abs(current_velocity - this->current_goal_velocity);
-  if (start_and_stop && velocity_error < 0.2) {
+  if (start_and_stop && abs(current_velocity - this->current_goal_velocity) < 0.2) {
     if (this->current_goal_velocity == 0) {
       this->current_goal_velocity = this->ideal_velocity;
     } else {
       this->current_goal_velocity = 0;
     }
+  }
+  if (!start_and_stop) {
+    this->current_goal_velocity = 0;
   }
 }
