589b389b297e90a0fd0b43c579f8a25e1ab7c109
Davide64-dev <davidpcu@hotmail.com>
2024-05-01 16:14:10 +0100

    fix: split performance test

--- src/planning/test/tests.cpp
+++ src/planning/test/tests.cpp
@@ -690,24 +690,3 @@ void extractInfo(const std::string_view& filenameView, int& size, int& n_outlier
     std::istringstream(size_str) >> size;
     std::istringstream(n_outliers_str) >> n_outliers;
 }
-
-
-/**
- * @brief Execution Time Test
- */
-TEST(LocalPathPlanner, delauney) {
-    std::string directory_path = "../../src/planning/test/maps/";
-    int size;
-    int n_outliers;
-    for (const auto& entry : fs::directory_iterator(directory_path)) {
-        if (fs::is_regular_file(entry.path())) {
-            std::string filename = entry.path().filename().string();
-            if (filename.find("map_") != std::string::npos) {
-                extractInfo(filename, size, n_outliers);
-                std::string filePath = "src/planning/test/maps/" + filename;
-                outlierCalculations(filePath, n_outliers);
-                std::vector<PathPoint> path = processTriangulations(filePath);
-            }
-        }
-    }
-}
