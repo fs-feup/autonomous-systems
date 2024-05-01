04bdd791e21106e70b65e17fe4a8e9e80e739d4a
marhcouto <marhcouto@users.noreply.github.com>
2024-05-01 17:56:32 +0000

    deploy: 40bdfc9eb99e863e8ab59e8c4ae722120d613639

--- dc/d59/classevaluator_1_1evaluator_1_1_evaluator.js
+++ dc/d59/classevaluator_1_1evaluator_1_1_evaluator.js
@@ -5,11 +5,11 @@ var classevaluator_1_1evaluator_1_1_evaluator =
     [ "perception_callback_time_measurement", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a70831dca93f611102095496f5c9bbb61", null ],
     [ "adapter", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a2fa1e1b009fdaf753356c9df194a6a63", null ],
     [ "end_time", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a78e9c198a50fbb5da2e72eda7de6a404", null ],
-    [ "perception_callback_time_measurement", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a6e764e5d397976e5332bd02f7006381d", null ],
     [ "perception_execution_time", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a852e5ef345d69a769fc5bec79dea4008", null ],
     [ "perception_inter_cones_distance", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#adb115c1419b125ee41f7959a7f7ad05d", null ],
     [ "perception_mean_difference", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a3d1ebc548b29e038a2ed9721b2e093c3", null ],
     [ "perception_mean_squared_difference", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#aef064cb20626a40b00579ff0afdc691c", null ],
+    [ "perception_output", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a24fe74d37f4cca6146ed9ee0b0e8458d", null ],
     [ "perception_root_mean_squared_difference", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a9997b79a78be19f887097666075b9656", null ],
     [ "perception_subscription", "dc/d59/classevaluator_1_1evaluator_1_1_evaluator.html#a5dc3df5998220a865365643c551d94a0", null ]
 ];
