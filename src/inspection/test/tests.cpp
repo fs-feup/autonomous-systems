e9e51a8ccf9b05a9b0610acfc54c4e384df9c816
lourenco31 <150086800+lourenco31@users.noreply.github.com>
2024-05-01 16:15:53 +0100

    FSF-3540 Inspection fix (#281)
    * fix(inspection): fixes and minor refactor
    
    * fix: removed line added automatically by vscode extension to .vscode
    
    * fix(inspection): default constructor for functions and test fixed
    
    * fix(inspection): maximum angle back to 30 degrees
    
    * fix(inspection): sonarcloud issues
    
    * sonarcloud issues
    
    * fix(inpection) : publishes emergency message at the end of ebs test
    
    * update submodule
    
    * fix(inspection): sonarcloud issue
    
    * update submodules and solved conflicts
    
    * feat(inspection) : throttle values are between -1 and 1
    
    * update submodules
    
    * fix(inspection) : sonarcloud issues
    
    * fix(inspection) : fixed 'intentionality' issues in tests
    
    * fix sonarcloud issues with 'new'
    
    * fix(inspection) : error in throttle conversion to adequate range
    
    * fix error caused by ros extension

--- src/inspection/test/tests.cpp
+++ src/inspection/test/tests.cpp
@@ -25,7 +25,7 @@ double round(double number, int n) {
  *
  */
 TEST(STEERING, steering1) {
-  InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   for (float i = 0; i < 26; i = i + 0.1) {
     double steering = new_inspection->calculate_steering(i);
@@ -39,7 +39,7 @@ TEST(STEERING, steering1) {
  *
  */
 TEST(TORQUE, torque1) {
-  InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double initial_velocity = 5;
   // i represents time
@@ -54,7 +54,7 @@ TEST(TORQUE, torque1) {
  *
  */
 TEST(TORQUE, torque2) {
-  InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double initial_velocity = -4;
   // i represents time
@@ -69,7 +69,7 @@ TEST(TORQUE, torque2) {
  *
  */
 TEST(TORQUE, torque3) {
-  InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double initial_velocity = 1.1;
   // i represents time
@@ -86,11 +86,10 @@ TEST(TORQUE, torque3) {
 TEST(TORQUE, torque4) {
   double max_angle = 3.14159265358979323846264338327950288 / 6.0, ideal_velocity = 10.0,
          turn_time = 0, wheel_radius = 0.254, gain = 0.5, stop_time = 30;
-  InspectionFunctions *new_inspection = new InspectionFunctions(
-      max_angle, turn_time, wheel_radius, stop_time, false, gain, ideal_velocity);
+  auto new_inspection = std::make_unique<InspectionFunctions>(
+      max_angle, turn_time, wheel_radius, stop_time, true, gain, ideal_velocity);
 
   double current_velocity = 0, max_velocity = -1, min_velocity = 1000;
-  bool accelerating = true;
   int count = 0;
   // i represents time
   for (float i = 0; i < stop_time; i = i + 0.1) {
@@ -102,16 +101,14 @@ TEST(TORQUE, torque4) {
     current_velocity += 0.1 * (new_inspection->calculate_throttle(current_velocity));
 
     // check goal has been reached and flip the goal
-    if (round(current_velocity, 2) == new_inspection->ideal_velocity) {
-      new_inspection->redefine_goal_velocity(accelerating ? 0.0 : 10.0);
-      accelerating = false;
+    if (fabs(current_velocity - new_inspection->current_goal_velocity) < 0.2) {
+      new_inspection->redefine_goal_velocity(current_velocity);
       count += 1;  // count how many times the ideal velocity changes
     }
   }
-  delete new_inspection;
   EXPECT_GT(count, 2);  // test if the ideal velocity changes the right number of times
-  EXPECT_EQ(round(min_velocity, 2), 0.0);
-  EXPECT_DOUBLE_EQ(round(max_velocity, 2), 10.0);
+  EXPECT_EQ(round(min_velocity, 1), 0.0);
+  EXPECT_DOUBLE_EQ(round(max_velocity, 1), 9.80);
 }
 
 /**
@@ -119,7 +116,7 @@ TEST(TORQUE, torque4) {
  *
  */
 TEST(CONVERSION, conversion1) {
-  InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double rpm = 15;
   EXPECT_DOUBLE_EQ(0.39898226700590372, new_inspection->rpm_to_velocity(rpm));
