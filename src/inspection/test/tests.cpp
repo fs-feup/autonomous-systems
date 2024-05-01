1155e9fc9c7693006c69d85db4241c8eaf49c205
lourenco31 <r.lourenco.a@gmail.com>
2024-05-01 15:32:30 +0100

    fix sonarcloud issues with 'new'

--- src/inspection/test/tests.cpp
+++ src/inspection/test/tests.cpp
@@ -25,7 +25,7 @@ double round(double number, int n) {
  *
  */
 TEST(STEERING, steering1) {
-  const InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   for (float i = 0; i < 26; i = i + 0.1) {
     double steering = new_inspection->calculate_steering(i);
@@ -39,7 +39,7 @@ TEST(STEERING, steering1) {
  *
  */
 TEST(TORQUE, torque1) {
-  const InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double initial_velocity = 5;
   // i represents time
@@ -54,7 +54,7 @@ TEST(TORQUE, torque1) {
  *
  */
 TEST(TORQUE, torque2) {
-  const InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double initial_velocity = -4;
   // i represents time
@@ -69,7 +69,7 @@ TEST(TORQUE, torque2) {
  *
  */
 TEST(TORQUE, torque3) {
-  const InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double initial_velocity = 1.1;
   // i represents time
@@ -86,7 +86,7 @@ TEST(TORQUE, torque3) {
 TEST(TORQUE, torque4) {
   double max_angle = 3.14159265358979323846264338327950288 / 6.0, ideal_velocity = 10.0,
          turn_time = 0, wheel_radius = 0.254, gain = 0.5, stop_time = 30;
-  InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       max_angle, turn_time, wheel_radius, stop_time, true, gain, ideal_velocity);
 
   double current_velocity = 0, max_velocity = -1, min_velocity = 1000;
@@ -106,7 +106,6 @@ TEST(TORQUE, torque4) {
       count += 1;  // count how many times the ideal velocity changes
     }
   }
-  delete new_inspection;
   EXPECT_GT(count, 2);  // test if the ideal velocity changes the right number of times
   EXPECT_EQ(round(min_velocity, 1), 0.0);
   EXPECT_DOUBLE_EQ(round(max_velocity, 1), 9.80);
@@ -117,7 +116,7 @@ TEST(TORQUE, torque4) {
  *
  */
 TEST(CONVERSION, conversion1) {
-  const InspectionFunctions *new_inspection = new InspectionFunctions(
+  auto new_inspection = std::make_unique<InspectionFunctions>(
       3.14159265358979323846264338327950288 / 6.0, 4.0, 0.254, 26, false, 0.5, 1.0);
   double rpm = 15;
   EXPECT_DOUBLE_EQ(0.39898226700590372, new_inspection->rpm_to_velocity(rpm));
