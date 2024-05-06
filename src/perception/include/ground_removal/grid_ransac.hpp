b6d97e58bf2d731b92a032a9d4957255dc55b428
Davide64-dev <davidpcu@hotmail.com>
2024-05-06 23:05:04 +0100

    feat(perception): grid RANSAC parallelized by OpenMP

--- src/perception/include/ground_removal/grid_ransac.hpp
+++ src/perception/include/ground_removal/grid_ransac.hpp
@@ -6,6 +6,7 @@
 #include "ground_removal/ground_removal.hpp"
 #include "ground_removal/ransac.hpp"
 
+
 class GridRANSAC : public GroundRemoval {
  private:
   RANSAC ransac;
@@ -18,24 +19,8 @@ class GridRANSAC : public GroundRemoval {
   void groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud,
                      pcl::PointCloud<pcl::PointXYZI>::Ptr ret, Plane& plane) const override;
 
-                
-  /**
-   * @brief Get a sub point cloud from the input point cloud given two angles and two radii.
-   *
-   * This function extracts a sub point cloud from the input point cloud based on two angles
-   * and two radius.
-   *
-   * @param[in] input_cloud The input point cloud.
-   * @param[out] sub_cloud The sub point cloud extracted based on the specified angles and radii.
-   * @param[in] start_angle The starting angle for the sub point cloud (in radians).
-   * @param[in] end_angle The ending angle for the sub point cloud (in radians).
-   * @param[in] min_radius The minimum radius for the sub point cloud.
-   * @param[in] max_radius The maximum radius for the sub point cloud.
-   */
-  static void getSubPointCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud,
-                        pcl::PointCloud<pcl::PointXYZI>::Ptr sub_cloud,
-                        double start_angle, double end_angle,
-                        double min_radius, double max_radius);
-
   static double getFarthestPoint(const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud);
+
+  void split_point_cloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud, 
+                                std::vector<std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr>>& grids) const;
 };
--- src/perception/src/ground_removal/grid_ransac.cpp
+++ src/perception/src/ground_removal/grid_ransac.cpp
@@ -9,27 +9,12 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <utils/plane.hpp>
+#include <cmath>
 
 GridRANSAC::GridRANSAC(double epsilon, int n_tries, int n_angular_grids, double radius_resolution) : 
         n_angular_grids(n_angular_grids), radius_resolution(radius_resolution) {
 
     this->ransac = RANSAC(epsilon, n_tries);
-}
-
-void GridRANSAC::getSubPointCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud,
-                        pcl::PointCloud<pcl::PointXYZI>::Ptr sub_cloud,
-                        double start_angle, double end_angle,
-                        double min_radius, double max_radius) {
-    sub_cloud->clear();
-
-    for (const auto& point : *input_cloud) {
-      double radius = std::sqrt(point.x * point.x + point.y * point.y);
-      double angle = std::atan2(point.y, point.x);
-      if (radius >= min_radius && radius <= max_radius &&
-          angle >= start_angle && angle <= end_angle) {
-        sub_cloud->push_back(point);
-      }
-    }
 }
 
 double GridRANSAC::getFarthestPoint(const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud) {
@@ -46,32 +31,61 @@ double GridRANSAC::getFarthestPoint(const pcl::PointCloud<pcl::PointXYZI>::Ptr& 
     return max_distance;
 }
 
-void GridRANSAC::groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud,
-                           pcl::PointCloud<pcl::PointXYZI>::Ptr ret, Plane& plane) const {
+void GridRANSAC::split_point_cloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud, 
+                       std::vector<std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr>>& grids) const {
+    grids = {};
 
-    ret->clear();
+    double max_distance = GridRANSAC::getFarthestPoint(cloud);
 
     double angle_increment = 360.0 / n_angular_grids;
 
-    double max_distance = GridRANSAC::getFarthestPoint(point_cloud);
+    // Matrix Initialization
+    for (int radius = 0; radius < max_distance; radius += radius_resolution){
+       std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> row = {};
+        for (int i = 0; i < n_angular_grids; i++){
+            pcl::PointCloud<pcl::PointXYZI>::Ptr pc_row;
+            row.push_back(pc_row);
+        }
+        grids.push_back(row);
+    }
+
+    // Matrix Population
+    for (auto point : *cloud){
+        double radius = std::sqrt(point.x * point.x + point.y * point.y);
+        double angle = std::atan2(point.y, point.x);
+        int row = static_cast<int>(radius / radius_resolution);
+        int column = static_cast<int>(angle / angle_increment);
+
+        grids[row][column]->points.push_back(point);
+    }
+
+}
+
+void GridRANSAC::groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud,
+                           pcl::PointCloud<pcl::PointXYZI>::Ptr ret, Plane& plane) const {
+    
+    // reseting everything
+    ret->clear();
+    plane = Plane(0,0,0,0);
+
+    std::vector<std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr>> grids;
+    split_point_cloud(point_cloud, grids); // Get the grids organized in a matrix format
 
     int count = 0;
 
-    // Add multithreading here
-    for (int ang = 0; ang < 360; ang += angle_increment){
-        for (int radius = 0; radius < max_distance; radius += radius_resolution){
-            pcl::PointCloud<pcl::PointXYZI>::Ptr sub_point_cloud;
-            GridRANSAC::getSubPointCloud(point_cloud, sub_point_cloud, ang, 
-                                         ang + angle_increment, radius, radius + radius_resolution);
-            
+    // Multithreading using OpenMP
+    #pragma omp parallel for reduction(+:count) schedule(dynamic)
+    for (int i = 0; i < grids.size(); ++i){
+        for (int j = 0; j < grids[i].size(); ++j){
             Plane grid_plane;
             pcl::PointCloud<pcl::PointXYZI>::Ptr grid_ret;
-            this->ransac.groundRemoval(point_cloud, grid_ret, grid_plane);
-
-            *ret = *ret + *grid_ret;
-
-            plane += grid_plane;
-            count++;
+            this->ransac.groundRemoval(grids[i][j], grid_ret, grid_plane);
+            #pragma omp critical
+            {
+                *ret += *grid_ret;
+                plane += grid_plane;
+                count++;
+            }
         }
     }
 
