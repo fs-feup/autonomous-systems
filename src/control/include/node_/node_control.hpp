5c72bcf779434834fe3808dbd93bd2ef1cd2940f
Yves Bonneau <yves.bonneau2004@gmail.com>
2024-05-07 15:08:42 +0000

    feat(control): done node control subscribers, and oublishers, using Cache message filter to look up most recent path point array when new pose is received

--- src/control/include/node_/node_control.hpp
+++ src/control/include/node_/node_control.hpp
@@ -1,12 +1,15 @@
+#include <message_filters/cache.h>
+#include <message_filters/subscriber.h>
+
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
 
 #include "adapter_control/adapter.hpp"
-#include "custom_interfaces/msg/control_command.h"
-#include "custom_interfaces/msg/path_point_array.h"
-#include "custom_interfaces/msg/pose.h"
+#include "custom_interfaces/msg/control_command.hpp"
+#include "custom_interfaces/msg/path_point_array.hpp"
+#include "custom_interfaces/msg/pose.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
@@ -21,29 +24,20 @@ class Adapter;
  */
 class Control : public rclcpp::Node {
  private:
-  message_filters::Cache<custom_interfaces::msg::PathPointArray> path_cache;
-  message_filters::Subscriber<custom_interfaces::msg::PathPointArray> path_point_array_sub;
   message_filters::Subscriber<custom_interfaces::msg::Pose> pose_sub;
+  message_filters::Subscriber<custom_interfaces::msg::PathPointArray> path_point_array_sub;
+  message_filters::Cache<custom_interfaces::msg::PathPointArray> path_cache;
 
-  // TODO: change to correct message type
   rclcpp::Publisher<custom_interfaces::msg::ControlCommand>::SharedPtr result;
 
   Adapter *adapter;
   std::string mode = "fsds";  // Temporary, change as desired. TODO(andre): Make not hardcoded
 
   /**
-   * @brief Publishes the steering angle to the car based on the path and pose using timer
-   * synchronizer
-   */
-  void publish_steering_angle_synchronized(
-      const custom_interfaces::msg::PathPointArray::ConstSharedPtr &path_msg,
-      const custom_interfaces::msg::Pose::ConstSharedPtr &pose_msg);
-
-  /**
    * @brief Publishes the steering angle to the car based on the path and pose using cache
-   * 
+   *
    */
-  void publish_steering_angle_cached(const custom_interfaces::msg::Pose::ConstSharedPtr &pose_msg);
+  void publish_steering_angle(const custom_interfaces::msg::Pose::ConstSharedPtr &pose_msg);
 
  public:
   /**
--- src/control/src/node_/node_control.cpp
+++ src/control/src/node_/node_control.cpp
@@ -1,9 +1,5 @@
 #include "node_/node_control.hpp"
 
-#include <message_filters/cache.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -19,39 +15,27 @@
 // This creates a subclass of Node and uses std::bind()
 // to define which function gets executed at each time
 
-// This function is called when a new path and pose are received
-void Control::publish_steering_angle_synchronized(
-    const custom_interfaces::msg::PathPointArray::ConstSharedPtr &path_msg,
-    const custom_interfaces::msg::Pose::ConstSharedPtr &pose_msg) {
-  // BARROS FILL IN
-}
-
 // This function is called when a new pose is received
-void Control::publish_steering_angle_cached(
-    const custom_interfaces::msg::Pose::ConstSharedPtr &pose_msg) {
-  // TODO: Fix missing header from pose_msg
+void Control::publish_steering_angle(const custom_interfaces::msg::Pose::ConstSharedPtr &pose_msg) {
   auto path_point_array = path_cache.getElemBeforeTime(pose_msg->header.stamp);
-  // BARROS FILL IN
+  auto control_msg = custom_interfaces::msg::ControlCommand();
+
+  // BARROS FILL IN USING pose_msg and path_point_array, then fill in the control_msg
+
+  result->publish(control_msg);
 }
 
 // TODO: Change to correct topics
 Control::Control()
     : Node("control"),
-      path_point_array_sub(this, "path_topic"),
-      pose_sub(this, "pose_topic"),
+      pose_sub(this, "/state_estimation/vehicle_state"),
+      path_point_array_sub(this, "/path_planning/path"),
       path_cache(path_point_array_sub, 10) {
-  // Approach with Time Synchronized
-  message_filters::TimeSynchronizer<custom_interfaces::msg::PathPointArray,
-                                    custom_interfaces::msg::Pose>
-      sync(path_point_array_sub, pose_sub, 10);
-  sync.registerCallback(&publish_steering_angle_synchronized, this);
-
-  // Approach with Cache
-  pose_sub.registerCallback(&publish_steering_angle_cached, this);
+  pose_sub.registerCallback(&Control::publish_steering_angle, this);
 
   // creates publisher that should yield torque/acceleration/...
   // TODO: change to correct message type
-  result = this->create_publisher<std_msgs::msg::String>("torque_topic", 10);
+  result = this->create_publisher<custom_interfaces::msg::ControlCommand>("/as_msgs/controls", 10);
 
   // Adapter to communicate with the car
   this->adapter = adapter_map[mode](this);
