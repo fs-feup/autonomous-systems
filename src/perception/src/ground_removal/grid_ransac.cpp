7e921be0518a249644d175cc647645248a9a74c5
Davide64-dev <davidpcu@hotmail.com>
2024-05-23 09:52:19 +0100

    test(perception): adapt Normal RANSAC tests for Grid RANSAC

--- src/perception/src/ground_removal/grid_ransac.cpp
+++ src/perception/src/ground_removal/grid_ransac.cpp
@@ -33,52 +33,64 @@ double GridRANSAC::get_furthest_point(const pcl::PointCloud<pcl::PointXYZI>::Ptr
 
 void GridRANSAC::split_point_cloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud, 
                        std::vector<std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr>>& grids) const {
-    grids = {};
+    
+    grids.clear();  // Clear any existing content in grids
 
     double max_distance = GridRANSAC::get_furthest_point(cloud);
 
     double angle_increment = 360.0 / n_angular_grids;
 
     // Matrix Initialization
-    for (int radius = 0; radius < max_distance; radius += radius_resolution){
-       std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> row = {};
-        for (int i = 0; i < n_angular_grids; i++){
-            pcl::PointCloud<pcl::PointXYZI>::Ptr pc_row;
+    for (int radius = 0; radius < max_distance; radius += radius_resolution) {
+        std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> row;
+        for (int i = 0; i < n_angular_grids; i++) {
+            pcl::PointCloud<pcl::PointXYZI>::Ptr pc_row(new pcl::PointCloud<pcl::PointXYZI>());
             row.push_back(pc_row);
         }
         grids.push_back(row);
     }
 
     // Matrix Population
-    for (auto point : *cloud){
+    for (const auto& point : *cloud) {
         double radius = std::sqrt(point.x * point.x + point.y * point.y);
-        double angle = std::atan2(point.y, point.x);
+        double angle = std::atan2(point.y, point.x) * 180.0 / M_PI;  // Convert to degrees
+
+        // Normalize angle to be within [0, 360)
+        if (angle < 0) {
+            angle += 360.0;
+        }
+
         int row = static_cast<int>(radius / radius_resolution);
-        int column = static_cast<int>(angle / angle_increment);
+        int column = static_cast<int>(angle / angle_increment) % n_angular_grids;
 
-        grids[row][column]->points.push_back(point);
+        if (row < grids.size()) {
+            grids[row][column]->points.push_back(point);
+        }
     }
-
 }
+
 
 void GridRANSAC::groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud,
                            pcl::PointCloud<pcl::PointXYZI>::Ptr ret, Plane& plane) const {
     
     // reseting everything
+
     ret->clear();
+    
     plane = Plane(0,0,0,0);
 
+    
     std::vector<std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr>> grids;
     split_point_cloud(point_cloud, grids); // Get the grids organized in a matrix format
 
     int count = 0;
-
+    
     // Multithreading using OpenMP
     #pragma omp parallel for reduction(+:count) schedule(dynamic)
     for (int i = 0; i < grids.size(); ++i){
         for (int j = 0; j < grids[i].size(); ++j){
             Plane grid_plane;
-            pcl::PointCloud<pcl::PointXYZI>::Ptr grid_ret;
+            pcl::PointCloud<pcl::PointXYZI>::Ptr grid_ret(new pcl::PointCloud<pcl::PointXYZI>);
             this->ransac.groundRemoval(grids[i][j], grid_ret, grid_plane);
             #pragma omp critical
             {
@@ -86,6 +98,7 @@ void GridRANSAC::groundRemoval(const pcl::PointCloud<pcl::PointXYZI>::Ptr point_
                 plane += grid_plane;
                 count++;
             }
+            
         }
     }
 
---
+++ src/perception/test/grid_ransac_test.cpp
@@ -0,0 +1,198 @@
+#include "ground_removal/grid_ransac.hpp"
+
+#include <gtest/gtest.h>
+
+#include <utils/plane.hpp>
+
+/**
+ * @brief Test class for setting up data and testing RANSAC algorithm.
+ *
+ */
+class GridRANSACTest : public ::testing::Test {
+ protected:
+  /**
+   * @brief Set up the test environment before each test case.
+   *
+   */
+  void SetUp() override {
+    pcl_cloud.reset(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl_cloud->points.push_back(pcl::PointXYZI{1.0, 0.0, 0.0, 0.5});
+    pcl_cloud->points.push_back(pcl::PointXYZI{0.0, 1.0, 0.0, 1.0});
+    pcl_cloud->points.push_back(pcl::PointXYZI{0.0, 0.0, 1.0, 1.5});
+    pcl_cloud->points.push_back(pcl::PointXYZI{0.0060, 0.0060, 0.0060, 2.0});
+    pcl_cloud->points.push_back(pcl::PointXYZI{10, 10, 10, 2.5});
+
+    pcl_cloud_empty.reset(new pcl::PointCloud<pcl::PointXYZI>);
+
+    pcl_cloud_3_points.reset(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl_cloud_3_points->points.push_back(pcl::PointXYZI{1.0, 0.0, 0.0, 0.5});
+    pcl_cloud_3_points->points.push_back(pcl::PointXYZI{0.0, 1.0, 0.0, 1.0});
+    pcl_cloud_3_points->points.push_back(pcl::PointXYZI{0.0, 0.0, 1.0, 1.5});
+  }
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud;
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud_empty;
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud_3_points;
+};
+
+/**
+ * @brief Test Scenario: All points fit in the model (Epsilon threshold very high).
+ *
+ */
+TEST_F(GridRANSACTest, TestBigEpsilon) {
+  auto ground_removal = new GridRANSAC(10000, 1, 1, 100);
+
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 0);
+}
+
+/**
+ * @brief Test Scenario: Only points that fit into the plane are considered as part of the plane (No
+ * points close enough to the plane).
+ *
+ */
+TEST_F(GridRANSACTest, TestCommonScenario) {
+  auto ground_removal = new GridRANSAC(0.05, 100, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 2);
+}
+
+/**
+ * @brief Test Scenario: The points in the plane and a close enough point is removed - 1 point left.
+ *
+ */
+TEST_F(GridRANSACTest, TestCommonScenario2) {
+  auto ground_removal = new GridRANSAC(0.5, 100, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 1);
+}
+
+/**
+ * @brief Test Scenario: The epsilon threshold is set to 0 - No points are removed.
+ *
+ */
+TEST_F(GridRANSACTest, TestThresholdZero) {
+  auto ground_removal = new GridRANSAC(0, 10, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 5);
+}
+
+/**
+ * @brief Test Scenario: Number of repetitions is set to 0 - Expected a point cloud with 0 points.
+ *
+ */
+TEST_F(GridRANSACTest, TestZeroRepetitions) {
+  auto ground_removal = new GridRANSAC(100, 0, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 0);
+}
+
+/**
+ * @brief Test Scenario: Really small threshold. Only the points of the plane are considered as part
+ * of the ground.
+ *
+ */
+TEST_F(GridRANSACTest, TestSmallEpsilon) {
+  auto ground_removal = new GridRANSAC(0.00000000000000001, 10, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 2);
+}
+
+/**
+ * @brief Test Scenario: Really great threshold - All points are considered as part of the plane.
+ *
+ */
+TEST_F(GridRANSACTest, TestBigEpsilon2) {
+  auto ground_removal = new GridRANSAC(1000000, 1, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 0);
+}
+
+/**
+ * @brief Test Scenario: Point cloud with only 3 points: 0 points are expected after the ground
+ * removal.
+ *
+ */
+TEST_F(GridRANSACTest, TestCommonScenario3Points) {
+  auto ground_removal = new GridRANSAC(100, 100, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud_3_points, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 0);
+}
+
+/**
+ * @brief Test Scenario: Point cloud with only 3 points: The epsilon threshold is set to 0 - No
+ * points are removed.
+ *
+ */
+TEST_F(GridRANSACTest, Test3PointsThresholdZero) {
+  auto ground_removal = new GridRANSAC(0, 100, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud_3_points, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 3);
+}
+
+/**
+ * @brief Test Scenario: Point cloud with 0 points - Must return a point with 0 points also.
+ *
+ */
+TEST_F(GridRANSACTest, TestEmptyPointCloud) {
+  auto ground_removal = new GridRANSAC(100, 100, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud_empty, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 0);
+}
+
+/**
+ * @brief Test Scenario: Point cloud with 0 points. Epsilon and repetitions set to 0 - Expected 0
+ * points.
+ *
+ */
+TEST_F(GridRANSACTest, TestEmptyPointCloud2) {
+  auto ground_removal = new GridRANSAC(0, 0, 1, 100);
+  Plane plane;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr ground_removed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  ground_removal->groundRemoval(pcl_cloud_empty, ground_removed_cloud, plane);
+
+  ASSERT_EQ(ground_removed_cloud->points.size(), 0);
+}
