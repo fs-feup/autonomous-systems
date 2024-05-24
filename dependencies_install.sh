9fb6d535fbefd62ade036c273b6c5e816bcf8a7f
marhcouto <marhcouto1101@gmail.com>
2024-05-24 12:08:37 +0000

    ref: switched to iron and removed gitkeeps

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -3,12 +3,12 @@
 sudo apt-get update && apt-get upgrade -y
 sudo apt-get install -y python3-pip
 sudo apt install python-is-python3 -y
-sudo apt install ros-humble-ackermann-msgs -y
+sudo apt install ros-iron-ackermann-msgs -y
 sudo apt install -y python3-pip
 sudo pip3 install colcon-common-extensions
 sudo apt install -y python3-rosdep
-sudo apt install -y ros-humble-message-filters
-sudo apt install -y ros-humble-tf-transformations
+sudo apt install -y ros-iron-message-filters
+sudo apt install -y ros-iron-tf-transformations
 
 # planning
 sudo apt-get install libcgal-dev -y
@@ -19,8 +19,8 @@ sudo apt install libpcl-dev -y
 sudo apt-get install -y ros-sensor-msgs -y
 sudo apt install libsensor-msgs-dev -y
 sudo apt install libpcl-conversions-dev -y
-sudo apt-get install ros-humble-pcl-conversions -y
-sudo apt-get install ros-humble-pcl-ros -y
+sudo apt-get install ros-iron-pcl-conversions -y
+sudo apt-get install ros-iron-pcl-ros -y
 sudo apt install rospack-tools -y
 rospack find sensor_msgs -y
 sudo apt-get install libpcap-dev -y
@@ -32,4 +32,4 @@ sudo apt-get install python3-matplotlib -y
 # evaluator
 sudo pip3 install transforms3d
 
-source /opt/ros/humble/setup.bash
+source /opt/ros/iron/setup.bash
---
+++ environment_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir ./cache/iron-ros-base-jammy/build
+mkdir ./cache/iron-ros-base-jammy/install
+mkdir ./cache/iron-ros-base-jammy/log
