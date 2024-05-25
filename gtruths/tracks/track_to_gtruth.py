98644a8c5c7ec57c3b704998966278aca0e64011
marhcouto <marhcouto1101@gmail.com>
2024-05-25 14:35:11 +0000

    fix: added pacsim planning gtruth

--- gtruths/tracks/track_to_gtruth.py
+++ gtruths/tracks/track_to_gtruth.py
@@ -4,9 +4,10 @@ import os
 import csv
 import argparse
 
+
 def read_csv(filename):
     initial_points = []
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         reader = csv.reader(file)
         next(reader)  # Skip header if present
         for row in reader:
@@ -14,20 +15,23 @@ def read_csv(filename):
             initial_points.append((float(x), float(y)))
     return initial_points
 
+
 class PointCollector:
     def __init__(self, initial_points):
         self.fig, self.ax = plt.subplots()
         self.points = list(initial_points)
         self.added_points = []  # Track the added points separately
-        self.ax.set_aspect('equal')
-        self.ax.set_title('Click to add points, drag to pan, scroll to zoom')
-        self.ax.scatter(*zip(*self.points), color='blue')
+        self.ax.set_aspect("equal")
+        self.ax.set_title("Click to add points, drag to pan, scroll to zoom")
+        self.ax.scatter(*zip(*self.points), color="blue")
         self.is_dragging = False
         self.clicked = False
-        self.cid_click = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
-        self.cid_scroll = self.fig.canvas.mpl_connect('scroll_event', self.onscroll)
-        self.cid_drag = self.fig.canvas.mpl_connect('motion_notify_event', self.onmove)
-        self.cid_release = self.fig.canvas.mpl_connect('button_release_event', self.onrelease)
+        self.cid_click = self.fig.canvas.mpl_connect("button_press_event", self.onclick)
+        self.cid_scroll = self.fig.canvas.mpl_connect("scroll_event", self.onscroll)
+        self.cid_drag = self.fig.canvas.mpl_connect("motion_notify_event", self.onmove)
+        self.cid_release = self.fig.canvas.mpl_connect(
+            "button_release_event", self.onrelease
+        )
         plt.show()
 
     def onclick(self, event):
@@ -36,60 +40,89 @@ class PointCollector:
 
     def onscroll(self, event):
         if event.inaxes == self.ax:
-            scale = 1.1 if event.button == 'up' else 1/1.1
-            self.ax.set_xlim(event.xdata - (event.xdata - self.ax.get_xlim()[0]) * scale,
-                             event.xdata + (self.ax.get_xlim()[1] - event.xdata) * scale)
-            self.ax.set_ylim(event.ydata - (event.ydata - self.ax.get_ylim()[0]) * scale,
-                             event.ydata + (self.ax.get_ylim()[1] - event.ydata) * scale)
+            scale = 1.1 if event.button == "up" else 1 / 1.1
+            self.ax.set_xlim(
+                event.xdata - (event.xdata - self.ax.get_xlim()[0]) * scale,
+                event.xdata + (self.ax.get_xlim()[1] - event.xdata) * scale,
+            )
+            self.ax.set_ylim(
+                event.ydata - (event.ydata - self.ax.get_ylim()[0]) * scale,
+                event.ydata + (self.ax.get_ylim()[1] - event.ydata) * scale,
+            )
             self.fig.canvas.draw()
             self.clicked = False
 
     def onmove(self, event):
         if event.inaxes == self.ax and event.button == 1:  # left button
             self.is_dragging = True
-            self.ax.set_xlim(self.ax.get_xlim() - (event.x - event.xdata),
-                             self.ax.get_xlim() - (event.x - event.xdata))
-            self.ax.set_ylim(self.ax.get_ylim() - (event.y - event.ydata),
-                             self.ax.get_ylim() - (event.y - event.ydata))
+            self.ax.set_xlim(
+                self.ax.get_xlim() - (event.x - event.xdata),
+                self.ax.get_xlim() - (event.x - event.xdata),
+            )
+            self.ax.set_ylim(
+                self.ax.get_ylim() - (event.y - event.ydata),
+                self.ax.get_ylim() - (event.y - event.ydata),
+            )
             self.fig.canvas.draw()
             self.clicked = False
 
     def onrelease(self, event):
         if event.inaxes == self.ax:
             if self.clicked and not self.is_dragging:
-                self.added_points.append((event.xdata, event.ydata))  # Track added points
-                self.ax.scatter(event.xdata, event.ydata, color='red')
+                self.added_points.append(
+                    (event.xdata, event.ydata)
+                )  # Track added points
+                self.ax.scatter(event.xdata, event.ydata, color="red")
                 self.fig.canvas.draw()
             self.clicked = False
             self.is_dragging = False
 
     def export_to_csv(self, filename):
-        df = pd.DataFrame(self.added_points, columns=['x', 'y'])  # Export only the added points
-        df['z'] = 0
+        df = pd.DataFrame(
+            self.added_points, columns=["x", "y"]
+        )  # Export only the added points
+        df["z"] = 0
         df.to_csv(filename, index=False)
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Create ground truth points for a track.")
-    parser.add_argument('--sim', type=str, default='fsds', help='Simulator type (fsds, eufs, pacsim)')
-    parser.add_argument('--track_name', type=str, default='track1.csv', help='Name of the track CSV file')
-    
+    parser = argparse.ArgumentParser(
+        description="Create ground truth points for a track."
+    )
+    parser.add_argument(
+        "--sim", type=str, default="fsds", help="Simulator type (fsds, eufs, pacsim)"
+    )
+    parser.add_argument(
+        "--track_name",
+        type=str,
+        default="track1.csv",
+        help="Name of the track CSV file",
+    )
+
     args = parser.parse_args()
     sim = args.sim
     track_name = args.track_name
 
     # Get the current directory
     current_directory = os.getcwd()
-    
+
     # Concatenate current directory with the provided filename
-    filename = os.path.join(current_directory, 'tracks', sim, track_name[:-4], track_name)
+    filename = os.path.join(
+        current_directory, "gtruths", "tracks", sim, track_name[:-4], track_name
+    )
 
     initial_points = read_csv(filename)
 
     pc = PointCollector(initial_points)
-    final_file_name = os.path.join('tracks', sim, track_name[:-4], f'{track_name[:-4]}_gtruth.csv')
-    
-    input(f"The following step will overwrite the file {final_file_name} \nTo stop this, press Ctrl + C followed by Enter; To continue, press Enter \n")
+    final_file_name = os.path.join(
+        "gtruths", "tracks", sim, track_name[:-4], f"{track_name[:-4]}_gtruth.csv"
+    )
+
+    input(
+        f"The following step will overwrite the file {final_file_name} \nTo stop this, press Ctrl + C followed by Enter; To continue, press Enter \n"
+    )
     pc.export_to_csv(final_file_name)
+
 
 if __name__ == "__main__":
     main()
