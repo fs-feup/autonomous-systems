7f79f9057d986ad165eb2a6fa6ac457cd503d330
PedromLBarros <pedromlbarros00@gmail.com>
2024-06-06 08:55:17 +0000

    fix(integration install): drivers cmd

--- dependencies_integration_install.sh
+++ dependencies_integration_install.sh
@@ -10,25 +10,25 @@ sudo apt-get -y install wget
 sudo apt-get -y install build-essential
 sudo apt-get -y install pkg-config 
 sudo apt install gcc-12
-wget --content-disposition "https://www.kvaser.com/downloads-kvaser/?utm_source=software&utm_ean=7330130980754&utm_status=latest"
-tar xvzf linuxcan.tar.gz
+wget --content-disposition "https://resources.kvaser.com/PreProductionAssets/Product_Resources/linuxcan_5_45_724.tar.gz"
+tar xvzf linuxcan_5_45_724.tar
 cd linuxcan
 make
 sudo make install 
-sudo make load
-cd ..
-sudo apt install -y libxml2-dev
-sudo apt install -y zlib1g-dev
-wget --content-disposition "https://www.kvaser.com/downloads-kvaser/?utm_source=software&utm_ean=7330130981966&utm_status=latest"
-tar xvzf kvlibsdk.tar.gz
-cd kvlibsdk
-make
-make check
-sudo make install
-cd ..
-rm -rf linuxcan
-rm -rf kvlibsdk
-rm linuxcan.tar.gz
-rm kvlibsdk.tar.gz
-rm linuxcan.tar.gz.1
-rm kvlibsdk.tar.gz.1
+# sudo make load
+# cd ..
+# sudo apt install -y libxml2-dev
+# sudo apt install -y zlib1g-dev
+# wget --content-disposition "https://www.kvaser.com/download/?utm_source=software&utm_ean=7330130980754&utm_status=latest"
+# tar xvzf kvlibsdk.tar.gz
+# cd kvlibsdk
+# make
+# make check
+# sudo make install
+# cd ..
+# rm -rf linuxcan
+# rm -rf kvlibsdk
+# rm linuxcan.tar.gz
+# rm kvlibsdk.tar.gz
+# rm linuxcan.tar.gz.1
+# rm kvlibsdk.tar.gz.1
