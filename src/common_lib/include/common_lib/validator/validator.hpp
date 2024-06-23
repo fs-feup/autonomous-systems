4501737a0622b4afc6285c0d9fe582a32f43be29
Davide64-dev <davidpcu@hotmail.com>
2024-06-23 16:26:11 +0100

    feat: created validator class for validating msgs

--- src/common_lib/include/common_lib/validator/validator.hpp
+++ src/common_lib/include/common_lib/validator/validator.hpp
@@ -2,18 +2,26 @@
 
 #include <string>
 #include <chrono>
+#include "common_lib/validator/assertions/assertion.hpp"
+#include <vector>
+#include <memory>
 
 namespace common_lib::validator {
 
+template<typename T>
 class Validator {
 private:
     double _expected_frequency_;
-    std::chrono::time_point<std::chrono::system_clock> _last_timestamp; // Store the last timestamp
+    std::chrono::time_point<std::chrono::system_clock> _last_timestamp;
+    std::vector<std::shared_ptr<assertions::Assertion<T>>> _assertions_;
+    T _last_message_;
 
 public:
-    Validator(double expected_frequency);
+    Validator(double expected_frequency, std::vector<std::shared_ptr<assertions::Assertion<T>>> assertions);
 
-    bool verifyFrequency();
+    bool verify(T msg, std::vector<std::string>& reports);
+
+    bool verify_frequency();
 };
 
 } // namespace common_lib::validator
--- src/common_lib/src/validator/validator.cpp
+++ src/common_lib/src/validator/validator.cpp
@@ -2,12 +2,14 @@
 
 namespace common_lib::validator {
 
-Validator::Validator(double expected_frequency)
-    : _expected_frequency_(expected_frequency) {
+template<typename T>
+Validator<T>::Validator(double expected_frequency, std::vector<std::shared_ptr<assertions::Assertion<T>>> assertions)
+    : _expected_frequency_(expected_frequency), _assertions_(std::move(assertions)) {
     _last_timestamp = std::chrono::system_clock::now();
 }
 
-bool Validator::verifyFrequency() {
+template<typename T>
+bool Validator<T>::verify_frequency() {
     auto current_time = std::chrono::system_clock::now();
     auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(current_time - _last_timestamp).count();
     
@@ -16,6 +18,29 @@ bool Validator::verifyFrequency() {
     _last_timestamp = current_time;
 
     return (_expected_frequency_ * 0.8) <= actual_frequency && actual_frequency <= (_expected_frequency_ * 1.2);
+}
+
+template<typename T>
+bool Validator<T>::verify(T msg, std::vector<std::string>& reports) {
+    bool all_valid = true;
+
+    if (!verify_frequency()){
+        all_valid = false;
+        reports.push_back("Frequency different than expected");
+    }
+
+    for (const auto& assertion : _assertions_) {
+        if (!assertion->validate(msg)) {
+            all_valid = false;
+            reports.push_back(assertion->get_error_message());
+        }
+        if (!assertion->validate(msg, _last_message_)){
+            all_valid = false;
+            reports.push_back(assertion->get_error_message());
+        }
+    }
+    _last_message_ = msg;
+    return all_valid;
 }
 
 } // namespace common_lib::validator
