328a5c26ed5e105ec35973b1f63924b34603142f
andrelds11 <andreldsantos11@gmail.com>
2024-06-26 12:11:01 +0100

    feat(planning): coloring added to performance test

--- src/planning/test/performance_test.cpp
+++ src/planning/test/performance_test.cpp
@@ -2,6 +2,39 @@
 #include "rclcpp/rclcpp.hpp"
 #include "test_utils/utils.hpp"
 
+void iterate_coloring(std::string filename) {
+  std::vector<common_lib::structures::Cone> cones =
+      cone_vector_from_file(filename);
+  auto cone_coloring = ConeColoring();
+
+  std::ofstream measuresPath = openWriteFile("performance/exec_time/planning/planning_" +
+                                             get_current_date_time_as_string() + ".csv");
+
+  int no_iters = 100;
+  double total_time = 0;
+
+  // No_iters repetitions to get average
+  for (int i = 0; i < no_iters; i++) {
+    float orientation = atan2(cones[1].position.y - cones[0].position.y, cones[1].position.x - cones[0].position.x);
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+    auto colored_cones = cone_coloring.color_cones(cones, Pose(cones[0].position.x, cones[0].position.y - 1.5, orientation));
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    double elapsed_time_ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
+
+    total_time += elapsed_time_ms;
+  }
+
+  measuresPath << total_time / no_iters << ",";
+  measuresPath.close();
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Cones colored in average %f ms.",
+              (total_time / no_iters));
+}
+
 /**
  * @brief Iterates the Outliers algorithm repeatedly and measures the average time
  */
@@ -17,7 +50,7 @@ void iterate_outliers(std::string filename, int num_outliers = 0) {
       "performance/exec_time/planning/planning_" + get_current_date_time_as_string() + ".csv",
       "Number of Left Cones,Number of Right Cones,Number of "
       "Outliers,Outliers Removal Execution "
-      "Time,Triangulations Execution Time,Smoothing Execution Time");
+      "Time,Cone Coloring Execution Time,Triangulations Execution Time,Smoothing Execution Time");
 
   for (int i = 0; i < no_iters; i++) {
     track = track_from_file(filename);
@@ -119,6 +152,7 @@ TEST(Planning, planning_exec_time) {
         extractInfo(filename, size, n_outliers);
         std::string filePath = "src/planning/test/maps/" + filename;
         iterate_outliers(filePath, n_outliers);
+        iterate_coloring(filePath);
         std::vector<PathPoint> path = iterate_triangulations(filePath);
         iterate_smoothing(path);
       }
