09e1640e00de3046779697d36b53096f262e77ed
lourenco31 <150086800+lourenco31@users.noreply.github.com>
2024-09-12 09:46:24 +0100

    FSF-4427 feat(planning): optional outlier removal and path smoothing (#338)
    feat(planning): outlier removal and path smoothing are optional and whether to use them is determined in launch file

--- src/planning/include/config/smoothing_config.hpp
+++ src/planning/include/config/smoothing_config.hpp
@@ -21,9 +21,14 @@ struct PathSmoothingConfig {
    *
    */
   float coeffs_ratio_ = 3.0;
+  /**
+   * @brief flag to enable/disable the path smoothing
+   *
+   */
+  bool use_path_smoothing_ = true;
   PathSmoothingConfig() = default;
-  PathSmoothingConfig(int precision, int order, float coeffs_ratio)
-      : precision_(precision), order_(order), coeffs_ratio_(coeffs_ratio) {}
+  PathSmoothingConfig(int precision, int order, float coeffs_ratio, bool use_path_smoothing)
+      : precision_(precision), order_(order), coeffs_ratio_(coeffs_ratio), use_path_smoothing_(use_path_smoothing) {}
 };
 
 #endif  // SRC_PLANNING_INCLUDE_CONFIG_SMOOTHING_CONFIG_HPP_
