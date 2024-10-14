ac1b66497142435ab6fd3b249c6a261ed374d1bd
lourenco31 <150086800+lourenco31@users.noreply.github.com>
2024-10-14 09:46:06 +0100

    feat(ekf_state_est): new state estimation documentation
    feat(ekf_state_est): new state estimation documentation

--- src/ekf_state_est/include/kalman_filter/data_association.hpp
+++ src/ekf_state_est/include/kalman_filter/data_association.hpp
@@ -26,13 +26,28 @@ class DataAssociationModel {
    */
   // virtual bool valid_match(const float delta, const float distance_to_vehicle) const = 0;
 
-  float max_landmark_distance_; /**< Maximum deviation of the landmark position
-                                from the expected position when the landmark is
-                                perceived to be 1 meter away */
+  /** Maximum distance from the car to a cone for it to be considered in the State Estimation. Cones
+   * farther away will be ignored.*/
+  float max_landmark_distance_;
+
 protected:
   float get_max_landmark_distance() const;
 
 public:
+  /**
+   * @brief Associate the observed landmarks to the expected landmarks
+   * and update the state vector and the covariance matrix
+   * @param perception_map The observed landmarks
+   * @param _x_vector_ The state vector
+   * @param _p_matrix_ The covariance matrix
+   * @param matched_ids Vector in which the ids of the matched landmarks will be placed
+   * @param matched_cone_positions Vector in which the positions of the matched landmarks will be
+   * placed
+   * @param new_features Vector in which the positions of the new landmarks (unmatched landmarks)
+   * will be placed
+   * @param observation_model The observation model
+   * @return int Always 0
+   */
   virtual int associate_n_filter(const std::vector<common_lib::structures::Cone> &perception_map,
                                  Eigen::VectorXf &_x_vector_, Eigen::MatrixXf &_p_matrix_,
                                  std::vector<int> &matched_ids,
@@ -60,6 +75,20 @@ public:
   static float association_gate_;   /// normalized innovation squared gate
   static float new_landmark_gate_;  /// normalized distance gate (closest unmatched landmark)
 
+  /**
+   * @brief Associate the observed landmarks to the expected landmarks
+   * and update the state vector and the covariance matrix
+   * @param perception_map The observed landmarks
+   * @param _x_vector_ The state vector
+   * @param _p_matrix_ The covariance matrix
+   * @param matched_ids Vector in which the ids of the matched landmarks will be placed
+   * @param matched_cone_positions Vector in which the positions of the matched landmarks will be
+   * placed
+   * @param new_features Vector in which the positions of the new landmarks (unmatched landmarks)
+   * will be placed
+   * @param observation_model The observation model
+   * @return int Always 0
+   */
   int associate_n_filter(const std::vector<common_lib::structures::Cone> &perception_map,
                          Eigen::VectorXf &_x_vector_, Eigen::MatrixXf &_p_matrix_,
                          std::vector<int> &matched_ids,
@@ -67,6 +96,11 @@ public:
                          std::vector<Eigen::Vector2f> &new_features,
                          ObservationModel *observation_model) const override;
 
+  /**
+   * @brief Construct a new Max Likelihood object
+   * @param max_landmark_distance Maximum distance from the car to an observed landmark for
+   * it to be considered in the state estimation
+   */
   explicit MaxLikelihood(float max_landmark_distance);
 
   // FRIEND_TEST(DATA_ASSOCIATION_MODEL, VALID_MATCH_FUNC_PERFECT_MATCH);
