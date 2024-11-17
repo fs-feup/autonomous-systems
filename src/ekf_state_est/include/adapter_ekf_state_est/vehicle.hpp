9bbb4aed638063914e215ca757abcbdb2a629cab
Lourenço Rodrigues <up202306331@up.pt>
2024-11-17 00:04:03 +0000

    feat(ekf_state_est): vehicle adapter receives xsens imu data

--- src/ekf_state_est/include/adapter_ekf_state_est/vehicle.hpp
+++ src/ekf_state_est/include/adapter_ekf_state_est/vehicle.hpp
@@ -10,6 +10,7 @@
 #include "custom_interfaces/msg/operational_status.hpp"
 #include "custom_interfaces/msg/steering_angle.hpp"
 #include "custom_interfaces/msg/wheel_rpm.hpp"
+#include "geometry_msgs/msg/vector3_stamped.hpp"
 #include "std_srvs/srv/empty.hpp"
 #include "std_srvs/srv/trigger.hpp"
 
@@ -25,12 +26,18 @@ class VehicleAdapter : public Adapter {
   rclcpp::Subscription<custom_interfaces::msg::OperationalStatus>::SharedPtr
       _operational_status_subscription_;  ///< Subscriber for operational status
 
+  message_filters::Subscriber<geometry_msgs::msg::Vector3Stamped> _free_acceleration_subscription_;
+  message_filters::Subscriber<geometry_msgs::msg::Vector3Stamped> _angular_velocity_subscription_;
+
   message_filters::Subscriber<custom_interfaces::msg::ImuData> _roll_accx_imu_subscription_;
   message_filters::Subscriber<custom_interfaces::msg::ImuData> _yaw_accy_imu_subscription_;
 
   using ImuPolicy = message_filters::sync_policies::ApproximateTime<
       custom_interfaces::msg::ImuData,
       custom_interfaces::msg::ImuData>;  ///< Policy for synchronizing IMU data
+  using XsensImuPolicy = message_filters::sync_policies::ApproximateTime<
+      geometry_msgs::msg::Vector3Stamped,
+      geometry_msgs::msg::Vector3Stamped>;  ///< Policy for synchronizing Xsens IMU data
   using WheelSteerPolicy = message_filters::sync_policies::ApproximateTime<
       custom_interfaces::msg::WheelRPM, custom_interfaces::msg::WheelRPM,
       custom_interfaces::msg::SteeringAngle>;  ///< Policy for synchronizing wheel speeds and
@@ -39,6 +46,8 @@ class VehicleAdapter : public Adapter {
       _sync_;  ///< Synchronizer for wheel speeds and steering angle
 
   std::shared_ptr<message_filters::Synchronizer<ImuPolicy>> _imu_sync_;
+
+  std::shared_ptr<message_filters::Synchronizer<XsensImuPolicy>> _xsens_imu_sync_;
 
   rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr
       _finished_client_;  ///< Client for finished signal
@@ -61,6 +70,18 @@ class VehicleAdapter : public Adapter {
    */
   void imu_subscription_callback(const custom_interfaces::msg::ImuData& roll_accx_data,
                                  const custom_interfaces::msg::ImuData& yaw_accy_data);
+
+  /**
+   * @brief Xsens IMU subscription callback, which receives both free acceleration and angular
+   * velocity data separately and synchronizes them to then call State Estimation node's IMU
+   * callback
+   *
+   * @param free_acceleration_msg acceleration without gravity in x, y and z axis
+   * @param angular_velocity_msg angular velocity around x, y and z axis
+   */
+  void xsens_imu_subscription_callback(
+      const geometry_msgs::msg::Vector3Stamped::SharedPtr& free_acceleration_msg,
+      const geometry_msgs::msg::Vector3Stamped::SharedPtr& angular_velocity_msg);
 
 public:
   explicit VehicleAdapter(std::shared_ptr<SENode> se_node);
