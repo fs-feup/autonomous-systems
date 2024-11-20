3eba7433f7b8e0e647c6ec23334c65b121963178
DiogoProPrayer <diogoalexandrepinheiro@gmail.com>
2024-11-20 15:09:59 +0000

    Fixed velocity planning compilation errors

--- src/launcher/launch/pacsim-testing.launch.py
+++ src/launcher/launch/pacsim-testing.launch.py
@@ -63,6 +63,6 @@ def generate_launch_description():
             # evaluator_launch_description,
             planning_launch_description,
             control_launch_description,
-            mocker_node_launch_description,
+            #mocker_node_launch_description,
         ],
     )
