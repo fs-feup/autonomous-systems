931d0e2bcf0d1f99fd2bcf2313a37f28c5349d07
Lourenço Rodrigues <up202306331@up.pt>
2024-12-26 01:12:16 +0000

    feat: ve node has estimator

--- src/velocity_estimation/include/node/node.hpp
+++ src/velocity_estimation/include/node/node.hpp
@@ -2,10 +2,12 @@
 
 #include "adapters/parameters.hpp"
 #include "custom_interfaces/msg/velocities.hpp"
+#include "estimators/estimator.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class VENode : public rclcpp::Node {
   VEParameters _parameters_;
+  std::shared_ptr<VelocityEstimator> _velocity_estimator_;
   rclcpp::Publisher<custom_interfaces::msg::Velocities>::SharedPtr _velocities_pub_;
 
 public:
