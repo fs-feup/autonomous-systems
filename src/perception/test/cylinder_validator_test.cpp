8f25e7e69e2776c3cf87ed02d8443de3ad7f82c4
Flopes55 <flopes.sp55@gmail.com>
2024-12-30 12:35:32 +0000

    Finished cylinder tests

--- src/perception/test/cylinder_validator_test.cpp
+++ src/perception/test/cylinder_validator_test.cpp
@@ -74,3 +74,24 @@ TEST_F(CylinderValidatorTest, PointsFarOutsideCylinders) {
   ASSERT_NEAR(result[0], 0.0, 1e-6);  // Distance XY ratio is 0.
   ASSERT_LT(result[2], 1.0);          // Some points are outside the cylinder.
 }
+
+/**
+ * @brief Test case to validate if points are outside all cylinders.
+ */
+TEST_F(CylinderValidatorTest, PointsOutsideCylinders) {
+  CylinderValidator validator(0.5, 1.0, 0.7, 1.5, 0.5);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  point_cloud->points.push_back(pcl::PointXYZI{1.0, 1.0, 2.0, 0});
+  point_cloud->points.push_back(pcl::PointXYZI{0.6, 0.6, 0.9, 0});
+  point_cloud->points.push_back(pcl::PointXYZI{0.3, 0.3, 0.5, 0});
+
+  Cluster cylinderPointCloud = Cluster(point_cloud);
+
+  std::vector<double> result = validator.coneValidator(&cylinderPointCloud, plane);
+
+  ASSERT_GE(result[0], 0.5);  // Distance XY ratio is higher than cap.
+  ASSERT_LE(result[0], 1.0);  // Distance XY ratio is capped at 1.
+  ASSERT_GE(result[1], 0.5);  // Distance Z ratio is higher than cap.
+  ASSERT_LE(result[1], 1.0);  // Distance Z ratio is capped at 1.
+  ASSERT_LT(result[2], 1.0);  // Some points are outside the cylinder.
+}
