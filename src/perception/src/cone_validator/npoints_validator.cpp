4280d5a0f146abfd1e18c2d1f5b80ea1fd824021
Flopes55 <flopes.sp55@gmail.com>
2025-01-12 22:16:48 +0000

    small fix

--- src/perception/src/cone_validator/npoints_validator.cpp
+++ src/perception/src/cone_validator/npoints_validator.cpp
@@ -4,7 +4,7 @@ NPointsValidator::NPointsValidator(long unsigned int min_n_points) : _min_n_poin
 
 std::vector<double> NPointsValidator::coneValidator(Cluster* cone_point_cloud,
                                                     [[maybe_unused]] Plane& plane) const {
-  // index 0 = if below minimum number of points 0, else 1. No confidence implemented to further
+  // index 0 = if below minimum number of points, else 1. No confidence implemented to further
   // punish small clusters.
   if (cone_point_cloud->get_point_cloud()->size() >= (double)_min_n_points_)
     return {1.0};
