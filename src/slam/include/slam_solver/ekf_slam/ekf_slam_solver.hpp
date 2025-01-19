2150c1c8749006b15c443e964d48f0b0a93481c4
Lourenço Rodrigues <up202306331@up.pt>
2025-01-19 01:04:30 +0000

    feat(slam): added comments to ekf slam functions

--- src/slam/include/slam_solver/ekf_slam/ekf_slam_solver.hpp
+++ src/slam/include/slam_solver/ekf_slam/ekf_slam_solver.hpp
@@ -16,7 +16,24 @@ class EKFSLAMSolver : public SLAMSolver {
   bool velocities_received_ = false;
   bool cones_receieved_ = false;
 
+  /**
+   * @brief Executed to deal with new velocity data
+   *
+   * @param velocities
+   */
   void add_motion_prior(const common_lib::structures::Velocities& velocities) override;
+
+  /**
+   * @brief executed when velocity data is received. Prediction step of the EKF
+   * which is meant to capture changes in the state of the system
+   *
+   * @param state vector with position and orientation, followed by the landmark positions {car_x,
+   * car_y, car_theta, x_cone_1, y_cone_1, x_cone_2, y_cone_2, ...}
+   * @param covariance
+   * @param process_noise_matrix estimated process noise
+   * @param last_update last time velocity data was received
+   * @param velocities new velocity data
+   */
   void predict(Eigen::VectorXd& state, Eigen::MatrixXd& covariance,
                const Eigen::MatrixXd& process_noise_matrix,
                const std::chrono::high_resolution_clock::time_point last_update,
