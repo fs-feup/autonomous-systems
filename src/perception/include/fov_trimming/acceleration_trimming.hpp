d466c4ecacf508540a246723cd7194a949e21103
Flopes55 <flopes.sp55@gmail.com>
2025-01-31 16:11:39 +0000

    Initial acceleration

---
+++ src/perception/include/fov_trimming/acceleration_trimming.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <pcl/PCLPointField.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include "fov_trimming/fov_trimming.hpp"
+
+class AccelerationTrimming : public FovTrimming {
+private:
+  double pc_max_range = 12, pc_min_range = 1, pc_rlidar_max_height = -0.22, pc_max_y;
+
+public:
+  /**
+   * @brief Constructor for the Acceleration Point Cloud Trimming algorithm.
+   * @param pc_max_range Maximum point cloud distance after trimming.
+   * @param pc_min_range Minimum point cloud distance after trimming.
+   * @param pc_rlidar_max_height Maximum point cloud height after trimming.
+   * @param pc_max_y Maximum lateral distance after trimming.
+   */
+  AccelerationTrimming(double pc_max_y);
+
+  /**
+   * @brief Default constructor.
+   *
+   * This constructor is provided as a default constructor.
+   */
+  AccelerationTrimming() = default;
+
+  /**
+   * @brief Perform ground removal on the input point cloud.
+   *
+   * This pure virtual function must be implemented by derived classes.
+   *
+   * @param point_cloud The input point cloud to be processed (trimmed).
+   * @param[out] ret The resulting point cloud after trimming.
+   */
+  void fov_trimming(pcl::PointCloud<pcl::PointXYZI>::Ptr point_cloud) const override;
+};
---
+++ src/perception/src/fov_trimming/acceleration_trimming.cpp
@@ -0,0 +1,43 @@
+#include "fov_trimming/acceleration_trimming.hpp"
+
+#include <pcl/PCLPointField.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+AccelerationTrimming::AccelerationTrimming(double pc_max_y)
+    : pc_max_range(pc_max_range),
+      pc_min_range(pc_min_range),
+      pc_rlidar_max_height(pc_rlidar_max_height),
+      pc_max_y(pc_max_y) {}
+
+void AccelerationTrimming::fov_trimming(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud) const {
+  pcl::PointCloud<pcl::PointXYZI>::Ptr trimmed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+
+  for (auto& point : cloud->points) {
+    double x = point.x;
+    double y = point.y;
+
+    // This rotates 90º:
+    point.x = -y;
+    point.y = x;
+
+    // Calculate distance LIDAR on the x0y plane
+    double distance = std::sqrt((point.x) * (point.x) + point.y * point.y);
+
+    // Calculate the angle of point in the XY plane in deegres
+    double angle = std::atan2(point.y, point.x) * 180 / M_PI;
+
+    if (point.z >= pc_rlidar_max_height || distance > pc_max_range ||
+        distance <= pc_min_range) {  // Ignore points too close or higher than a
+                                     // defined height relative
+      continue;                      // to the LIDAR's position
+    }
+
+    // Check if the point is within the specified distance, angle range and lateral distance
+    if (angle >= -90 && angle <= 90 && pc_max_y >= point.y && -pc_max_y <= point.y) {
+      trimmed_cloud->points.push_back(point);
+    }
+  }
+
+  *cloud = *trimmed_cloud;
+}
