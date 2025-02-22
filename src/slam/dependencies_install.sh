4fb2318fb949a762605bfa61884c047360f8e986
Davide64-dev <davidpcu@hotmail.com>
2025-02-22 15:13:08 +0000

    fix: solved dependencies_install.sh

--- src/slam/dependencies_install.sh
+++ src/slam/dependencies_install.sh
@@ -2,6 +2,3 @@
 sudo apt-get install libboost-all-dev
 
 cd ./ext/gtsam && mkdir build && cd build && cmake -DGTSAM_USE_SYSTEM_EIGEN=ON -DCMAKE_PREFIX_PATH="/usr/share/eigen3" .. && sudo make check && sudo make install && cd ../../..
-
-pip install transforms3d==0.4.1
-pip install scipy==1.10.0
