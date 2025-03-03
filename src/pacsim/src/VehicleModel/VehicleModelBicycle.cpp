93b5e10d83a5803fa55e490034183170b1ef59ff
Yves Bonneau <yves.bonneau2004@gmail.com>
2025-03-03 17:36:01 +0000

    split model into car components removed magic numbers (most)

--- src/pacsim/src/VehicleModel/VehicleModelBicycle.cpp
+++ src/pacsim/src/VehicleModel/VehicleModelBicycle.cpp
@@ -1,9 +1,9 @@
 #include "VehicleModel/VehicleModelInterface.hpp"
-#include "VehicleModel/VehicleModeConfig.hpp"
 #include "VehicleModel/VehicleModelBicycle.hpp"
 #include <cmath>
 
 #include "transform.hpp"
+
 VehicleModelBicycle::VehicleModelBicycle()
     : position(Eigen::Vector3d::Zero()),
       orientation(Eigen::Vector3d::Zero()),
@@ -26,26 +26,26 @@ bool VehicleModelBicycle::readConfig(ConfigElement& config)
 
     // Read tire model parameters
     auto tire = configModel["tire"];
-    tire.getElement<double>(&Blat, "Blat");
-    tire.getElement<double>(&Clat, "Clat");
-    tire.getElement<double>(&Dlat, "Dlat");
-    tire.getElement<double>(&Elat, "Elat");
+    tire.getElement<double>(&tireModel.Blat, "Blat");
+    tire.getElement<double>(&tireModel.Clat, "Clat");
+    tire.getElement<double>(&tireModel.Dlat, "Dlat");
+    tire.getElement<double>(&tireModel.Elat, "Elat");
 
     // Read aerodynamic parameters
     auto aero = configModel["aero"];
-    aero.getElement<double>(&cla, "cla");
-    aero.getElement<double>(&cda, "cda");
-    aero.getElement<double>(&aeroArea, "aeroArea");
+    aero.getElement<double>(&aeroModel.cla, "cla");
+    aero.getElement<double>(&aeroModel.cda, "cda");
+    aero.getElement<double>(&aeroModel.aeroArea, "aeroArea");
 
     // Read general vehicle parameters
     configModel.getElement<double>(&m, "m");
     configModel.getElement<double>(&Izz, "Izz");
-    configModel.getElement<double>(&wheelRadius, "wheelRadius");
-    configModel.getElement<double>(&gearRatio, "gearRatio");
-    configModel.getElement<double>(&innerSteeringRatio, "innerSteeringRatio");
-    configModel.getElement<double>(&outerSteeringRatio, "outerSteeringRatio");
-    configModel.getElement<double>(&nominalVoltageTS, "nominalVoltageTS");
-    configModel.getElement<double>(&powerGroundForce, "powerGroundForce");
+    configModel.getElement<double>(&powertrainModel.wheelRadius, "wheelRadius");
+    configModel.getElement<double>(&powertrainModel.gearRatio, "gearRatio");
+    configModel.getElement<double>(&steeringModel.innerSteeringRatio, "innerSteeringRatio");
+    configModel.getElement<double>(&steeringModel.outerSteeringRatio, "outerSteeringRatio");
+    configModel.getElement<double>(&powertrainModel.nominalVoltageTS, "nominalVoltageTS");
+    configModel.getElement<double>(&powertrainModel.powerGroundForce, "powerGroundForce");
     
     return true;
 }
@@ -87,33 +87,17 @@ Wheels VehicleModelBicycle::getSteeringAngles()
 
 double VehicleModelBicycle::getSteeringWheelAngle()
 {
-    // Calculate steering wheel angle based on the front left steering angle and appropriate ratio
-    return (steeringAngles.FL > 0) ? 
-           steeringAngles.FL / innerSteeringRatio : 
-           steeringAngles.FL / outerSteeringRatio;
+    return steeringModel.calculateSteeringWheelAngle(steeringAngles);
 }
 
 double VehicleModelBicycle::getVoltageTS()
 {
-    return nominalVoltageTS;
+    return powertrainModel.nominalVoltageTS;
 }
 
 double VehicleModelBicycle::getCurrentTS()
 {
-    constexpr double powerCoeff = 1.0 / 9.55;
-    
-    // Calculate power for each wheel
-    double powerFL = torques.FL * wheelspeeds.FL * powerCoeff;
-    double powerFR = torques.FR * wheelspeeds.FR * powerCoeff;
-    double powerRL = torques.RL * wheelspeeds.RL * powerCoeff;
-    double powerRR = torques.RR * wheelspeeds.RR * powerCoeff;
-    
-    // Calculate powertrain efficiency based on rear wheel torques
-    double powertrainEfficiency = 1.0;
-    
-    // Calculate total power and return current
-    double totalPower = (powerFL + powerFR + powerRL + powerRR) / powertrainEfficiency;
-    return (totalPower / nominalVoltageTS);
+    return powertrainModel.calculateCurrent(torques, wheelspeeds, powertrainModel.nominalVoltageTS);
 }
 
 Wheels VehicleModelBicycle::getWheelspeeds()
@@ -153,7 +137,7 @@ void VehicleModelBicycle::setMinTorques(Wheels in)
 
 void VehicleModelBicycle::setSteeringSetpointFront(double in)
 {
-    setSteeringFront(in);
+    steeringModel.calculateSteeringAngles(in, steeringAngles);
 }
 
 void VehicleModelBicycle::setSteeringSetpointRear(double in)
@@ -171,20 +155,6 @@ void VehicleModelBicycle::setPowerGroundSetpoint(double in)
     powerGroundSetpoint = std::clamp(in, 0.0, 1.0);
 }
 
-void VehicleModelBicycle::setSteeringFront(double in)
-{
-    // Apply Ackermann steering geometry
-    double avgRatio = 0.5 * (innerSteeringRatio + outerSteeringRatio);
-    
-    if (in > 0) {
-        steeringAngles.FL = innerSteeringRatio * in / avgRatio;
-        steeringAngles.FR = outerSteeringRatio * in / avgRatio;
-    } else {
-        steeringAngles.FL = outerSteeringRatio * in / avgRatio;
-        steeringAngles.FR = innerSteeringRatio * in / avgRatio;
-    }
-}
-
 void VehicleModelBicycle::setPosition(Eigen::Vector3d newPosition)
 {
     position = newPosition;
@@ -195,7 +165,19 @@ void VehicleModelBicycle::setOrientation(Eigen::Vector3d newOrientation)
     orientation = newOrientation;
 }
 
-int VehicleModelBicycle::sign(double value)
+double VehicleModelBicycle::processSlipAngleLat(double alpha_input, double Fz)
+{
+    return tireModel.calculateLateralForce(alpha_input, Fz);
+}
+
+void VehicleModelBicycle::AerodynamicsModel::calculateForces(double velocityX, double& downforce, double& drag) const
+{
+    double velocitySquared = velocityX * velocityX;
+    downforce = 0.5 * AIR_DENSITY * aeroArea * cla * velocitySquared;
+    drag = 0.5 * AIR_DENSITY * aeroArea * cda * velocitySquared;
+}
+
+int VehicleModelBicycle::TireModel::sign(double value) const
 {
     if (value > 0)
         return 1;
@@ -204,72 +186,159 @@ int VehicleModelBicycle::sign(double value)
     return 1;  // Default to 1 for zero
 }
 
-double VehicleModelBicycle::processSlipAngleLat(double alpha_input, double Fz)
-{
-    using namespace VehicleModelConstants;
-
+double VehicleModelBicycle::TireModel::calculateLateralForce(double slipAngle, double normalForce) const
+{   
     double dpi = (p_input - NOMPRES) / NOMPRES;
     double Fz_0_prime = LFZO * FNOM;
     double Kya = PKY1 * Fz_0_prime * (1 + PPY1 * dpi) * (1 - PKY3 * abs(y_input))
-        * sin(PKY4 * atan((Fz / Fz_0_prime) / ((PKY2 + PKY5 * y_input * y_input) * (1 + PPY2 * dpi)))) * zeta_3
+        * sin(PKY4 * atan((normalForce / Fz_0_prime) / ((PKY2 + PKY5 * y_input * y_input) * (1 + PPY2 * dpi)))) * zeta_3
         * LKY;
-    double dfz = (Fz - Fz_0_prime) / Fz_0_prime;
-    double Kyg0 = Fz * (PKY6 + PKY7 * dfz) * (1 + PPY5 * dpi) * LKYC;
-    double Vs_y = tan(alpha_input) * abs(V_cx);
+    double dfz = (normalForce - Fz_0_prime) / Fz_0_prime;
+    double Kyg0 = normalForce * (PKY6 + PKY7 * dfz) * (1 + PPY5 * dpi) * LKYC;
+    double Vs_y = tan(slipAngle) * abs(V_cx);
     double Vs_x = -slip_ratio * abs(V_cx);
     double Vs = sqrt(Vs_x * Vs_x + Vs_y * Vs_y);
     double V0 = LONGVL;
     double LMUY_star = LMUY / (1.0 + LMUV * (Vs / V0));
-    double SVyg = Fz * (PVY3 + PVY4 * dfz) * y_input * LKYC * LMUY_star * zeta_2;
+    double SVyg = normalForce * (PVY3 + PVY4 * dfz) * y_input * LKYC * LMUY_star * zeta_2;
     double SHy
         = (PHY1 + PHY2 * dfz) + ((Kyg0 * y_input - SVyg) / (Kya + eps_k * sign(Kya))) * zeta_0 + zeta_4 - 1.0;
-    double alpha_y = alpha_input + SHy;
+    double alpha_y = slipAngle + SHy;
     double Ey
         = (PEY1 + PEY2 * dfz) * (1 + PEY5 * y_input * y_input - (PEY3 + PEY4 * y_input) * sign(alpha_y)) * LEY;
     double Cy = PCY1 * LCY;
     double mu_y = (PDY1 + PDY2 * dfz) * (1.0 + PPY3 * dpi + PPY4 * dpi * dpi) * (1.0 - PDY3 * y_input * y_input)
         * LMUY_star;
-    double Dy = mu_y * Fz * zeta_2;
+    double Dy = mu_y * normalForce * zeta_2;
     double By = Kya / (Cy * Dy + eps_y * sign(Dy));
-    double DVyk = mu_y * Fz * (RVY1 + RVY2 * dfz + RVY3 * y_input) * cos(atan(RVY4 * alpha_input)) * zeta_2;
+    double DVyk = mu_y * normalForce * (RVY1 + RVY2 * dfz + RVY3 * y_input) * cos(atan(RVY4 * slipAngle)) * zeta_2;
     double SVyk = DVyk * sin(RVY5 * atan(RVY6 * slip_ratio)) * LVYKA;
     double Eyk = REY1 + REY2 * dfz;
     double SHyk = RHY1 + RHY2 * dfz;
     double ks = slip_ratio + SHyk;
-    double Byk = (RBY1 + RBY4 * y_input * y_input) * cos(atan(RBY2 * (alpha_input - RBY3))) * LYKA;
+    double Byk = (RBY1 + RBY4 * y_input * y_input) * cos(atan(RBY2 * (slipAngle - RBY3))) * LYKA;
     double Cyk = RCY1;
     double Gyk_0 = cos(Cyk * atan(Byk * SHyk - Eyk * (Byk * SHyk - atan(Byk * SHyk))));
     double Gyk = (cos(Cyk * atan(Byk * ks - Eyk * (Byk * ks - atan(Byk * ks)))) / Gyk_0);
-    double SVy = Fz * (PVY1 + PVY2 * dfz) * LVY * LMUY_star * zeta_2 + SVyg;
+    double SVy = normalForce * (PVY1 + PVY2 * dfz) * LVY * LMUY_star * zeta_2 + SVyg;
     double Fy_0 = Dy * sin(Cy * atan(By * alpha_y - Ey * (By * alpha_y - atan(By * alpha_y)))) + SVy;
     double Fy = Gyk * Fy_0 + SVyk;
 
     return Fy;
 }
 
+void VehicleModelBicycle::PowertrainModel::calculateWheelTorques(double throttleInput, Wheels& torques) const
+{
+    // Simple rear-wheel drive implementation
+    torques.FL = 0;
+    torques.FR = 0;
+    torques.RL = throttleInput * MAX_TORQUE * 0.5;
+    torques.RR = throttleInput * MAX_TORQUE * 0.5;
+}
+
+double VehicleModelBicycle::PowertrainModel::calculateEfficiency(const Wheels& torques) const
+{
+    // Simple efficiency model based on rear wheel torques
+    return 0.002333 * (torques.RL + torques.RR) + 0.594;
+}
+
+double VehicleModelBicycle::PowertrainModel::calculateCurrent(const Wheels& torques, const Wheels& wheelspeeds, double voltage) const
+{
+    constexpr double powerCoeff = 1.0 / 9.55;
+    
+    // Calculate power for each wheel
+    double powerFL = torques.FL * wheelspeeds.FL * powerCoeff;
+    double powerFR = torques.FR * wheelspeeds.FR * powerCoeff;
+    double powerRL = torques.RL * wheelspeeds.RL * powerCoeff;
+    double powerRR = torques.RR * wheelspeeds.RR * powerCoeff;
+    
+    // Calculate total power and return current
+    double totalPower = (powerFL + powerFR + powerRL + powerRR) / calculateEfficiency(torques);
+    return (totalPower / voltage);
+}
+
+void VehicleModelBicycle::SteeringModel::calculateSteeringAngles(double steeringInput, Wheels& steeringAngles) const
+{
+    // Apply Ackermann steering geometry
+    double avgRatio = 0.5 * (innerSteeringRatio + outerSteeringRatio);
+    
+    if (steeringInput > 0) {
+        steeringAngles.FL = innerSteeringRatio * steeringInput / avgRatio;
+        steeringAngles.FR = outerSteeringRatio * steeringInput / avgRatio;
+    } else {
+        steeringAngles.FL = outerSteeringRatio * steeringInput / avgRatio;
+        steeringAngles.FR = innerSteeringRatio * steeringInput / avgRatio;
+    }
+    
+    // Rear wheels have zero steering angle
+    steeringAngles.RL = 0.0;
+    steeringAngles.RR = 0.0;
+}
+
+double VehicleModelBicycle::SteeringModel::calculateSteeringWheelAngle(const Wheels& steeringAngles) const
+{
+    // Calculate steering wheel angle based on the front left steering angle and appropriate ratio
+    return (steeringAngles.FL > 0) ? 
+           steeringAngles.FL / innerSteeringRatio : 
+           steeringAngles.FL / outerSteeringRatio;
+}
+
+void VehicleModelBicycle::calculateNormalForces(double& Fz_Front, double& Fz_Rear) const
+{
+    double l = lr + lf;
+    
+    // Calculate downforce from aerodynamics
+    double downforce = 0.0;
+    double drag = 0.0;
+    aeroModel.calculateForces(velocity.x(), downforce, drag);
+    
+    // Add ground effect if requested
+    downforce += powerGroundSetpoint * powertrainModel.powerGroundForce;
+    
+    // Calculate normal forces on axles with weight distribution and aero effects
+    Fz_Front = std::max(0.0, (m * GRAVITY+ downforce) * 0.5 * lr / l);
+    Fz_Rear = std::max(0.0, (m * GRAVITY + downforce) * 0.5 * lf / l);
+}
+
+void VehicleModelBicycle::calculateSlipAngles(double& kappaFront, double& kappaRear) const
+{
+    constexpr double eps = 0.00001; // Prevent division by zero
+    bool stillstand = (velocity.norm() < VELOCITY_THRESHOLD) && (std::abs(angularVelocity.z()) < ANGULAR_VELOCITY_THRESHOLD);
+    
+    // Calculate wheel positions relative to CoG
+    Eigen::Vector3d rFront(lf, 0.0, 0.0);
+    Eigen::Vector3d rRear(-lr, 0.0, 0.0);
+    
+    // Calculate wheel velocities
+    Eigen::Vector3d vFront = velocity + angularVelocity.cross(rFront);
+    Eigen::Vector3d vRear = velocity + angularVelocity.cross(rRear);
+    
+    // Calculate average front steering angle
+    double steeringFront = 0.5 * (steeringAngles.FL + steeringAngles.FR);
+    
+    // Calculate slip angles for front and rear
+    kappaFront = stillstand ? 0.0 : 
+                std::atan2(vFront.y(), std::max(std::abs(vFront.x()), eps)) - steeringFront;
+    kappaRear = stillstand ? 0.0 : 
+               std::atan2(vRear.y(), std::max(std::abs(vRear.x()), eps));
+}
+
 Eigen::Vector3d VehicleModelBicycle::getDynamicStates(double dt)
 {
-    constexpr double g = 9.81;
-    constexpr double airDensity = 1.29;
 
     // Calculate average front steering angle
     double steeringFront = 0.5 * (steeringAngles.FL + steeringAngles.FR);
     
-    // Calculate total vehicle length
-    double l = lr + lf;
-    
-    // Calculate aerodynamic forces
-    double velocitySquared = velocity.x() * velocity.x();
-    double F_aero_downforce = 0.5 * airDensity * aeroArea * cla * velocitySquared + 
-                               powerGroundSetpoint * powerGroundForce;
-    double F_aero_drag = 0.5 * airDensity * aeroArea * cda * velocitySquared;
-    
     // Calculate normal forces on axles
-    double Fz_Front = std::max(0.0, (m * g + F_aero_downforce) * 0.5 * lr / l);
-    double Fz_Rear = std::max(0.0, (m * g + F_aero_downforce) * 0.5 * lf / l);
+    double Fz_Front, Fz_Rear;
+    calculateNormalForces(Fz_Front, Fz_Rear);
 
+    // Calculate aero drag
+    double downforce, drag;
+    aeroModel.calculateForces(velocity.x(), downforce, drag);
+    
     // Calculate rolling resistance (applied opposite to velocity direction)
-    double frict = (Fz_Front + Fz_Rear) * 0.015;
+    double frict = (Fz_Front + Fz_Rear) * ROLLING_RESISTANCE_COEFF;
     Eigen::Vector3d friction = (velocity.norm() < 0.01) ?
                                Eigen::Vector3d::Zero() :
                                Eigen::Vector3d(frict, 0.0, 0.0);
@@ -287,38 +356,28 @@ Eigen::Vector3d VehicleModelBicycle::getDynamicStates(double dt)
     Eigen::Vector3d vFR = velocity + angularVelocity.cross(rFR);
     Eigen::Vector3d vRL = velocity + angularVelocity.cross(rRL);
     Eigen::Vector3d vRR = velocity + angularVelocity.cross(rRR);
-    Eigen::Vector3d vFront = velocity + angularVelocity.cross(rFront);
-    Eigen::Vector3d vRear = velocity + angularVelocity.cross(rRear);
-
+    
     // Conversion factor from RPM to m/s
-    double rpm2ms = wheelRadius * 2.0 * M_PI / 60;
-
-    // Check if vehicle is at standstill
-    bool stillstand = (velocity.norm() < 0.1) && (std::abs(angularVelocity.z()) < 0.001);
+    double rpm2ms = powertrainModel.wheelRadius * TWO_PI / 60;
 
     // Calculate powertrain efficiency
-    double powertrainEfficiency = 0.002333 * (torques.RL + torques.RR) + 0.594;
+    double powertrainEfficiency = powertrainModel.calculateEfficiency(torques);
     
     // Calculate longitudinal forces on wheels
     double Fx_FL = 0; // Front wheel drive not enabled
     double Fx_FR = 0; // Front wheel drive not enabled
-    double Fx_RL = (gearRatio * torques.RL / wheelRadius) * powertrainEfficiency;
-    double Fx_RR = (gearRatio * torques.RR / wheelRadius) * powertrainEfficiency;
+    double Fx_RL = (powertrainModel.gearRatio * torques.RL / powertrainModel.wheelRadius) * powertrainEfficiency;
+    double Fx_RR = (powertrainModel.gearRatio * torques.RR / powertrainModel.wheelRadius) * powertrainEfficiency;
     
     // Apply forces only when torque is significant or vehicle is moving
-    Fx_FL *= (torques.FL > 0.5 || velocity.x() > 0.3) ? 1.0 : 0.0;
-    Fx_FR *= (torques.FR > 0.5 || velocity.x() > 0.3) ? 1.0 : 0.0;
-    Fx_RL *= (torques.RL > 0.5 || velocity.x() > 0.3) ? 1.0 : 0.0;
-    Fx_RR *= (torques.RR > 0.5 || velocity.x() > 0.3) ? 1.0 : 0.0;
+    Fx_FL *= (torques.FL > TORQUE_THRESHOLD || velocity.x() > VELOCITY_MIN_THRESHOLD) ? 1.0 : 0.0;
+    Fx_FR *= (torques.FR > TORQUE_THRESHOLD || velocity.x() > VELOCITY_MIN_THRESHOLD) ? 1.0 : 0.0;
+    Fx_RL *= (torques.RL > TORQUE_THRESHOLD || velocity.x() > VELOCITY_MIN_THRESHOLD) ? 1.0 : 0.0;
+    Fx_RR *= (torques.RR > TORQUE_THRESHOLD || velocity.x() > VELOCITY_MIN_THRESHOLD) ? 1.0 : 0.0;
     
-    // Calculate tire slip angles
-    constexpr double eps = 0.00001; // Prevent division by zero
-    
-    // Calculate slip angles for front and rear
-    double kappaFront = stillstand ? 0.0 : 
-                        std::atan2(vFront.y(), std::max(std::abs(vFront.x()), eps)) - steeringFront;
-    double kappaRear = stillstand ? 0.0 : 
-                       std::atan2(vRear.y(), std::max(std::abs(vRear.x()), eps));
+    // Calculate slip angles
+    double kappaFront, kappaRear;
+    calculateSlipAngles(kappaFront, kappaRear);
     
     // Calculate lateral forces using tire model
     double Fy_Front = processSlipAngleLat(kappaFront, Fz_Front/2) * 2;
@@ -337,7 +396,7 @@ Eigen::Vector3d VehicleModelBicycle::getDynamicStates(double dt)
                       std::sin(steeringFront) * Fy_Front) / m;
                       
     // Apply aerodynamic drag and friction to get total acceleration
-    double axModel = axTires - F_aero_drag / m - friction.x() / m;
+    double axModel = axTires - drag / m - friction.x() / m;
     
     // Calculate lateral acceleration from tire forces
     double ayTires = (std::sin(steeringAngles.FL) * Fx_FL + 
@@ -368,13 +427,8 @@ void VehicleModelBicycle::forwardIntegrate(double dt)
     Eigen::AngleAxisd yawAngle(orientation.z(), Eigen::Vector3d::UnitZ());
     position += (yawAngle.matrix() * velocity) * dt;
     
-    // Convert throttle to wheel torques
-    // Rear wheel drive with even torque distribution
-    const double maxTorque = 120.0; // 120 Nm max total torque
-    torques.FL = 0;
-    torques.FR = 0;
-    torques.RL = throttleActuation * maxTorque * 0.5;
-    torques.RR = throttleActuation * maxTorque * 0.5;
+    // Convert throttle to wheel torques (using PowertrainModel)
+    powertrainModel.calculateWheelTorques(throttleActuation, torques);
     
     // Calculate dynamic states
     Eigen::Vector3d dynamicStates = getDynamicStates(dt);
@@ -393,16 +447,16 @@ void VehicleModelBicycle::forwardIntegrate(double dt)
     velocity += dt * (acceleration - angularVelocity.cross(velocity));
     
     // Update wheel orientations
-    const double wheelRotationFactor = 2.0 * M_PI / (60.0 * gearRatio);
+    const double wheelRotationFactor = TWO_PI / (60.0 * powertrainModel.gearRatio);
     
     wheelOrientations.FL = std::fmod(
-        wheelOrientations.FL + wheelspeeds.FL * dt * wheelRotationFactor, 2.0 * M_PI);
+        wheelOrientations.FL + wheelspeeds.FL * dt * wheelRotationFactor, TWO_PI);
     wheelOrientations.FR = std::fmod(
-        wheelOrientations.FR + wheelspeeds.FR * dt * wheelRotationFactor, 2.0 * M_PI);
+        wheelOrientations.FR + wheelspeeds.FR * dt * wheelRotationFactor, TWO_PI);
     wheelOrientations.RL = std::fmod(
-        wheelOrientations.RL + wheelspeeds.RL * dt * wheelRotationFactor, 2.0 * M_PI);
+        wheelOrientations.RL + wheelspeeds.RL * dt * wheelRotationFactor, TWO_PI);
     wheelOrientations.RR = std::fmod(
-        wheelOrientations.RR + wheelspeeds.RR * dt * wheelRotationFactor, 2.0 * M_PI);
+        wheelOrientations.RR + wheelspeeds.RR * dt * wheelRotationFactor, TWO_PI);
 }
 
 std::array<Eigen::Vector3d, 4> VehicleModelBicycle::getWheelPositions()
