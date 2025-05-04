fac395e8eeaf38439cc8d1697664f96735c59b3a
Lourenço Rodrigues <up202306331@up.pt>
2025-05-04 14:49:09 +0000

    Updated control adapters and fixed motion models

--- src/control/src/adapter_control/eufs.cpp
+++ src/control/src/adapter_control/eufs.cpp
@@ -1,5 +1,6 @@
 #include "adapter_control/eufs.hpp"
 
+#include "custom_interfaces/msg/pose.hpp"
 #include "node_/node_control.hpp"
 
 EufsAdapter::EufsAdapter(const ControlParameters& params)
@@ -20,18 +21,15 @@ EufsAdapter::EufsAdapter(const ControlParameters& params)
 
 void EufsAdapter::vehicle_state_callback(const eufs_msgs::msg::CarState& msg) {
   // Update the vehicle state
-  custom_interfaces::msg::VehicleState vehicle_state;
-  vehicle_state.position.x = msg.pose.pose.position.x;
-  vehicle_state.position.y = msg.pose.pose.position.y;
+  custom_interfaces::msg::Pose vehicle_state;
+  vehicle_state.x = msg.pose.pose.position.x;
+  vehicle_state.y = msg.pose.pose.position.y;
   vehicle_state.theta = atan2(2.0f * (msg.pose.pose.orientation.w * msg.pose.pose.orientation.z +
                                       msg.pose.pose.orientation.x * msg.pose.pose.orientation.y),
                               msg.pose.pose.orientation.w * msg.pose.pose.orientation.w +
                                   msg.pose.pose.orientation.x * msg.pose.pose.orientation.x -
                                   msg.pose.pose.orientation.y * msg.pose.pose.orientation.y -
                                   msg.pose.pose.orientation.z * msg.pose.pose.orientation.z);
-
-  vehicle_state.linear_velocity =
-      std::sqrt(std::pow(msg.twist.twist.linear.x, 2) + std::pow(msg.twist.twist.linear.y, 2));
 
   publish_control(vehicle_state);
 }
