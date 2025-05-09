2dae43d13a0a863bfd2d4276c9b707b286eaf5ee
Davide64-dev <davidpcu@hotmail.com>
2025-05-09 15:55:24 +0100

    feat: added run configuration

--- src/vehicle_dynamics_node/setup.py
+++ src/vehicle_dynamics_node/setup.py
@@ -19,7 +19,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": ["vehicle_dynamics_node = vehicle_dynamics_node.vehicle_dynamics_node:main"],
     },
 )
