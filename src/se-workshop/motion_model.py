44045b7ebc53fd8f84fe0ce67f21d85ffb290273
Leandro Resende <resendeleandro565@gmail.com>
2025-05-13 17:41:25 +0100

    Added motion model logic

--- src/se-workshop/motion_model.py
+++ src/se-workshop/motion_model.py
@@ -1,19 +1,9 @@
 import numpy as np
 
 def update_pose(pose, v, dt):
-    """
-    Update the pose of the robot using the motion model.
     
-    Args:
-        pose (np.array): Current pose of the robot [x, y, theta].
-        v (float): velocities [m/s] [vx, vy, omega] in the car's body frame!!!
-        dt (float): Time step.
-    
-    Returns:
-        np.array: Updated pose [x, y, theta].
-    """
-    # TODO: Implement the actual motion model
-    x_new = 0
-    y_new = 0
-    theta_new = 0
+    x_new = pose[0] + v[0] * dt * np.cos(pose[2]) - v[1] * dt * np.sin(pose[2])
+    y_new = pose[1] + v[0] * dt * np.sin(pose[2]) + v[1] * dt * np.cos(pose[2])
+    theta_new = pose[2] + v[2] * dt
+
     return np.array([x_new, y_new, theta_new], dtype=float)
