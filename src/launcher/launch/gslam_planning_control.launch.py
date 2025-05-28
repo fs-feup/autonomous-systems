0258fe557573fd7e6143f4715ab4a3cfa6f56e52
aqueiros-22 <up202205501@up.pt>
2025-05-28 20:56:54 +0000

    minor updates

---
+++ src/launcher/launch/gslam_planning_control.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    slam_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([FindPackageShare("slam"), "launch", "slam.launch.py"])
+        ),
+    )
+    evaluator_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("evaluator"), "launch", "evaluator.launch.py"]
+            )
+        ),
+    )
+    planning_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("planning"), "launch", "planning.launch.py"]
+            )
+        ),
+    )
+    control_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("control"), "launch", "control.launch.py"]
+            )
+        ),
+    )
+    return LaunchDescription(
+        [
+            slam_launch_description,
+            evaluator_launch_description,
+            planning_launch_description,
+            control_launch_description,
+        ],
+    )
