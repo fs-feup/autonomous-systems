a52e7213fcc18f073e04fd699823ad1de385dec7
Davide64-dev <davidpcu@hotmail.com>
2025-05-28 16:43:23 +0100

    fix: removed default from common lib

--- src/common_lib/include/common_lib/car_parameters/car_parameters.hpp
+++ src/common_lib/include/common_lib/car_parameters/car_parameters.hpp
@@ -15,7 +15,7 @@ struct CarParameters {
   double axis_length = 1.2;
   double dist_cg_2_rear_axis = 0.804;
   double gear_ratio = 4;
-  CarParameters() = default;
+  CarParameters();
   CarParameters(double wheel_diameter, double wheelbase, double rear_axis_to_camera,
                 double axis_length, double dist_cg_2_rear_axis, double gear_ratio);
 };
