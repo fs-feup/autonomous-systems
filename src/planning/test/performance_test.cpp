f94a42edbbaa6f821c48338cc0cc5412e9c309d2
Joana Louro <joana.az.louro@gmail.com>
2025-10-14 12:40:50 +0000

    remove useless tests

--- src/planning/test/performance_test.cpp
+++ src/planning/test/performance_test.cpp
@@ -38,41 +38,6 @@ void iterate_outliers(const std::string &filename, int num_outliers = 0) {
 }
 
 /**
- * @brief Iterates the Path Calculation algorithm repeatedly and measures the average time
- */
-// std::vector<PathPoint> iterate_triangulations(const std::string &filename) {
-//   auto track = track_from_file(filename);
-//   auto path_calculation = PathCalculation();
-
-//   std::vector<PathPoint> path;
-//   std::ofstream measures_path = openWriteFile("performance/exec_time/planning/planning_" +
-//                                               get_current_date_time_as_string() + ".csv");
-//   double total_time = 0;
-//   int no_iters = 100;
-
-//   // No_iters repetitions to get average
-//   for (int i = 0; i < no_iters; i++) {
-//     auto t0 = std::chrono::high_resolution_clock::now();
-
-//     path = path_calculation.process_delaunay_triangulations(track);
-
-//     auto t1 = std::chrono::high_resolution_clock::now();
-
-//     double elapsed_time_ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
-
-//     total_time += elapsed_time_ms;
-//   }
-
-//   measures_path << total_time / no_iters << ",";
-//   measures_path.close();
-
-//   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Average Delaunay Triangulations processed in %f ms.",
-//               (total_time / no_iters));
-
-//   return path;
-// }
-
-/**
  * @brief Iterates the Smoothing algorithm repeatedly and measures the average time
  */
 void iterate_smoothing(std::vector<PathPoint> &path) {
@@ -106,24 +71,3 @@ void iterate_smoothing(std::vector<PathPoint> &path) {
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Average Smoothing processed in %f ms.",
               (total_time / no_iters));
 }
-
-/**
- * @brief Execution Time Test
- */
-// TEST(Planning, planning_exec_time) {
-//   std::string directory_path = "../../src/planning/test/maps/";
-//   int size = 0;
-//   int n_outliers = 0;
-//   for (const auto &entry : fs::directory_iterator(directory_path)) {
-//     if (fs::is_regular_file(entry.path())) {
-//       std::string filename = entry.path().filename().string();
-//       if (filename.find("map_") != std::string::npos) {
-//         extract_info(filename, size, n_outliers);
-//         std::string filePath = "src/planning/test/maps/" + filename;
-//         iterate_outliers(filePath, n_outliers);
-//         std::vector<PathPoint> path = iterate_triangulations(filePath);
-//         iterate_smoothing(path);
-//       }
-//     }
-//   }
-// }
