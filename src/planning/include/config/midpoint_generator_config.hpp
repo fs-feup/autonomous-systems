b7fece85dfd317b5b926fb9dc9c29dc351b528f4
Joana <joana.az.louro@gmail.com>
2026-01-17 00:40:57 +0000

    solving the parameters problem

--- src/planning/include/config/midpoint_generator_config.hpp
+++ src/planning/include/config/midpoint_generator_config.hpp
@@ -8,19 +8,36 @@ struct MidpointGeneratorConfig {
   /**
    * @brief Minimum distance between cones.
    */
-  double minimum_cone_distance_ = 10.0;
+  double minimum_cone_distance_;
 
   /**
    * @brief Maximum distance between cones.
    */
-  double maximum_cone_distance_ = 2.0;
+  double maximum_cone_distance_;
 
   /**
    * @brief Sliding window radius for midpoint generation.
    */
-  double sliding_window_radius_ = 20.0;
+  double sliding_window_radius_;
 
-  MidpointGeneratorConfig() = default;
+  /**
+   * @brief Default constructor.
+   */
+  MidpointGeneratorConfig() 
+      : minimum_cone_distance_(2.0),
+        maximum_cone_distance_(10.0),
+        sliding_window_radius_(20.0) {}
+
+  /**
+   * @brief Parameterized constructor.
+   * @param min_distance Minimum distance between cones
+   * @param max_distance Maximum distance between cones
+   * @param window_radius Sliding window radius
+   */
+  MidpointGeneratorConfig(double min_distance, double max_distance, double window_radius)
+      : minimum_cone_distance_(min_distance),
+        maximum_cone_distance_(max_distance),
+        sliding_window_radius_(window_radius) {}
 };
 
 #endif  // SRC_PLANNING_INCLUDE_CONFIG_MIDPOINT_GENERATOR_CONFIG_HPP_
--- src/planning/include/config/skidpad_config.hpp
+++ src/planning/include/config/skidpad_config.hpp
@@ -3,22 +3,28 @@
 
 /**
  * @brief Configuration parameters for the Skidpad class.
- *
  */
 struct SkidpadConfig {
   /**
    * @brief Minimum number of cones required.
-   * 
    */
-  int minimum_cones_ = 10;
+  int minimum_cones_;
 
   /**
    * @brief Maximum distance for matching cone pairs during ICP alignment.
-   *
    */
-  double tolerance_ = 2.0;
+  double tolerance_;
 
-  SkidpadConfig() = default;
+  /**
+   * @brief Default constructor with sensible defaults.
+   */
+  SkidpadConfig() : minimum_cones_(10), tolerance_(2.0) {}
+
+  /**
+   * @brief Parameterized constructor.
+   */
+  SkidpadConfig(int minimum_cones, double tolerance)
+      : minimum_cones_(minimum_cones), tolerance_(tolerance) {}
 };
 
 #endif  // SRC_PLANNING_INCLUDE_CONFIG_SKIDPAD_CONFIG_HPP_
