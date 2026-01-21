5d16f750e9c7966b2fc4196ebc2c9c3e5f42bdea
FSFEUP <formulastudentfeup@gmail.com>
2026-01-21 16:34:52 +0000

    car integration

--- dependencies_integration_install.sh
+++ dependencies_integration_install.sh
@@ -10,8 +10,8 @@ sudo apt-get -y install wget
 sudo apt-get -y install build-essential
 sudo apt-get -y install pkg-config 
 sudo apt install gcc-12
-wget --content-disposition "https://resources.kvaser.com/PreProductionAssets/Product_Resources/linuxcan_5_45_724.tar.gz"
-tar xvzf linuxcan_5_45_724.tar.gz
+wget --content-disposition "https://www.kvaser.com/downloads-kvaser/?utm_source=software&utm_ean=7330130980754&utm_status=latest"
+tar xvzf linuxcan-5.50.312.tar.gz 
 cd linuxcan
 make
 sudo make install 
@@ -28,5 +28,5 @@ sudo make install
 cd ..
 rm -rf linuxcan
 rm -rf kvlibsdk
-rm linuxcan_5_45_724.tar.gz
-rm linuxcan_5_45_724.tar.gz.1
+rm linuxcan-5.50.312.tar.gz
+rm linuxcan-5.50.312.tar.gz.1
