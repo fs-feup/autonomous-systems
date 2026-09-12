83738e5425c37e091a43697225c4cb99d904bddb
Joana <joana.az.louro@gmail.com>
2026-09-12 21:38:12 +0000

    2 correction

--- src/planning/src/planning/planning.cpp
+++ src/planning/src/planning/planning.cpp
@@ -80,18 +80,32 @@ PlanningParameters Planning::load_config(std::string &adapter) {
   params.vp_use_velocity_planning_ = planning_config["vp_use_velocity_planning"].as<bool>();
   params.vp_desired_velocity_ = planning_config["vp_desired_velocity"].as<double>();
   params.vp_use_adaptive_velocity_ = planning_config["vp_use_adaptive_velocity"].as<bool>();
-  params.vp_curvature_peak_threshold_ = planning_config["vp_curvature_peak_threshold"]
-                                            ? planning_config["vp_curvature_peak_threshold"].as<double>()
-                                            : 0.05;
-  params.vp_min_section_spacing_ = planning_config["vp_min_section_spacing"]
-                                       ? planning_config["vp_min_section_spacing"].as<int>()
-                                       : 5;
-  params.vp_adaptive_anchor_mean_ = planning_config["vp_adaptive_anchor_mean"]
-                                        ? planning_config["vp_adaptive_anchor_mean"].as<std::vector<double>>()
-                                        : std::vector<double>{0.00, 0.05, 0.10, 0.15, 0.20, 0.30, 0.90, 1.00, 1.50};
-  params.vp_adaptive_anchor_delta_ = planning_config["vp_adaptive_anchor_delta"]
-                                         ? planning_config["vp_adaptive_anchor_delta"].as<std::vector<double>>()
-                                         : std::vector<double>{2.00, 1.50, 1.00, 0.85, 0.65, -0.20, -1.00, -1.25, -1.50};
+  if (planning_config["vp_curvature_peak_threshold"]) {
+    params.vp_curvature_peak_threshold_ =
+        planning_config["vp_curvature_peak_threshold"].as<double>();
+  } else {
+    params.vp_curvature_peak_threshold_ = 0.05;
+  }
+
+  if (planning_config["vp_min_section_spacing"]) {
+    params.vp_min_section_spacing_ = planning_config["vp_min_section_spacing"].as<int>();
+  } else {
+    params.vp_min_section_spacing_ = 5;
+  }
+
+  if (planning_config["vp_adaptive_anchor_mean"]) {
+    params.vp_adaptive_anchor_mean_ =
+        planning_config["vp_adaptive_anchor_mean"].as<std::vector<double>>();
+  } else {
+    params.vp_adaptive_anchor_mean_ = {0.00, 0.05, 0.10, 0.15, 0.20, 0.30, 0.90, 1.00, 1.50};
+  }
+
+  if (planning_config["vp_adaptive_anchor_delta"]) {
+    params.vp_adaptive_anchor_delta_ =
+        planning_config["vp_adaptive_anchor_delta"].as<std::vector<double>>();
+  } else {
+    params.vp_adaptive_anchor_delta_ = {2.00, 1.50, 1.00, 0.85, 0.65, -0.20, -1.00, -1.25, -1.50};
+  }
 
   /*--------------------- Planning Configuration Parameters --------------------*/
   params.planning_publishing_visualization_msgs_ =
@@ -423,6 +437,8 @@ void Planning::run_trackdrive() {
       // Continue tracking
     } else if (lap_counter_ >= 10) {
       velocity_planning_.stop(smoothed_path_, planning_config_.braking_distance_autocross_);
+    } else {
+      // Lap 0: initial lap before completion, continue tracking
     }
     return;
   }
